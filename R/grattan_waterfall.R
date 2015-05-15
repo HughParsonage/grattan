c8ffe2be6b42d776e54384decf2623b0cf2a5114
HughParsonage <hugh.parsonage@gmail.com>
2015-05-15 15:28:32 +1000

    added waterfall chart
    added `assign` to properly deal with assignment to the global
    environment

--- R/grattan_waterfall.R
+++ R/grattan_waterfall.R
@@ -1,25 +1,39 @@
 #' Create waterfall charts
 #' 
 #' @name grattan waterfall
+#' @param .data a placeholder for piped operations; cannot be used
 #' @param values a numeric vector making up the heights of the rectangles in the waterfall
 #' @param labels the labels corresponding to each vector, marked on the x-axis
+#' @param calc_total (logical) should the final pool of the waterfall be calculated (and placed on the chart)
+#' @param total_rect_color the color of the final rectangle
+#' @param total_rect_text_color the color of the final rectangle's label text
 #' @param fill_by_sign (logical) should positive and negative values each have the same colour?
 #' @param dark (logical) (only for fill_by_sign) should the dark palette be used?
 #' @param rect_width (numeric) the width of the rectangle, relative to the space between each label factor
-#' @param draw_lines (logical) should lines be drawn conneting successive rectangles
+#' @param rect_border the border around each rectangle. Choose NA if no border is desired.
+#' @param draw_lines (logical) should lines be drawn between successive rectangles
+#' @param linetype the linetype for the draw_lines
 #' @param lines_anchors a character vector of length two specifying the horizontal placement of the drawn lines relative to the preceding and successive rectangles, respectively
 #' @param ggplot_object_name (character) a quoted valid object name to which ggplot layers may be addded after the function has run
-#' @value a ggplot2 object and the object name visibly to the parent environment
 
 
-grattan_waterfall <- function(values, labels, 
+grattan_waterfall <- function(.data = NULL,
+                              values, labels, 
+                              calc_total = FALSE,
+                              total_rect_color = gpal(6)[1],
+                              total_rect_text_color = "white",
                               fill_colours = gpalx(length(values)),
                               fill_by_sign = TRUE,
                               dark = FALSE,
                               rect_width = 0.7,
+                              rect_border = "black",
                               draw_lines = TRUE,
                               lines_anchors = c("centre", "centre"),
-                              ggplot_object_name = "p"){
+                              linetype = "dashed",
+                              ggplot_object_name = "w"){
+  if(!is.null(.data))
+    warning(".data argument not yet supported")
+  
   if(!(length(values) == length(labels) && length(labels) == length(fill_colours)))
     stop("values, labels, and fill_colours must all  have same length")
   
@@ -51,10 +65,21 @@ grattan_waterfall <- function(values, labels,
   if (grepl("^r", lines_anchors[2]))
     anchor_right <- rect_width / 2
   
+  if (!calc_total){
   p <- ggplot2::ggplot(data.frame(x = labels,
                                   y = values), aes(x = x, y = y)) + 
     ggplot2::geom_blank() + 
-    grattan::theme_hugh() 
+    grattan::theme_hugh() +
+    ggplot2::theme(axis.title = element_blank())
+  } else {
+    p <- ggplot2::ggplot(data.frame(x = c(labels, "Total"),
+                                    y = c(values, north_edge[number_of_rectangles])
+                                    ), 
+                                    aes(x = x, y = y)) + 
+      ggplot2::geom_blank() + 
+      grattan::theme_hugh() +
+      ggplot2::theme(axis.title = element_blank())
+  }
   
   for (i in seq_along(values)){
     p <- p + ggplot2::annotate("rect",
@@ -62,6 +87,7 @@ grattan_waterfall <- function(values, labels,
                                xmax = i + rect_width/2,
                                ymin = south_edge[i],
                                ymax = north_edge[i],
+                               colour = rect_border,
                                fill = fill_colours[i])  +
       ggplot2::annotate("text",
                         x = i,
@@ -74,11 +100,38 @@ grattan_waterfall <- function(values, labels,
       p <- p + ggplot2::annotate("segment",
                                  x = i - 1 - anchor_left,
                                  xend = i + anchor_right,
+                                 linetype = linetype,
                                  y = south_edge[i],
                                  yend = south_edge[i])
     }
   }
+  if (calc_total){
+    p <- p + ggplot2::annotate("rect",
+                               xmin = number_of_rectangles + 1 - rect_width/2,
+                               xmax = number_of_rectangles + 1 + rect_width/2,
+                               ymin = 0,
+                               ymax = north_edge[number_of_rectangles],
+                               colour = rect_border,
+                               fill = total_rect_color)  +
+      ggplot2::annotate("text",
+                        x = number_of_rectangles + 1,
+                        y = 0.5 * north_edge[number_of_rectangles],
+                        label = ifelse(north_edge[number_of_rectangles] < 0,
+                                       paste0("\U2212", -1 * north_edge[number_of_rectangles]),
+                                       north_edge[number_of_rectangles]),
+                        color = total_rect_text_color,
+                        size = 7.14) + 
+      ggplot2::annotate("segment",
+                        x = number_of_rectangles - anchor_left,
+                        xend = number_of_rectangle + 1 + anchor_right,
+                        y = north_edge[number_of_rectangles],
+                        yend = north_edge[number_of_rectangles],
+                        linetype = linetype) + 
+      scale_x_discrete(labels = c(labels, "Total"))
+  } else {
+    p <- p + scale_x_discrete(labels = labels)
+  }
   print(p)
-  # Allow modifications beyond the function call.
-  "<<-"(ggplot_object_name, p)
+  # Allow modifications beyond the function call
+  assign(ggplot_object_name, p, inherits = TRUE)
 }
