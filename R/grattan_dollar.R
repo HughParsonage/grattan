3f444e50c4d75c94f6da711b19788d42364684cc
HughParsonage <hugh.parsonage@gmail.com>
2015-05-28 15:17:20 +1000

    grattan_dollar initial
    untested

---
+++ R/grattan_dollar.R
@@ -0,0 +1,15 @@
+#' dollar scales
+#' 
+#' @details makes negative ten Gs appears as -\$10,000 instead of \$-10,000
+
+# from scales
+grattan_dollar <- function (x) 
+{
+  #
+  commaz <- format(x, nsmall = nsmall, trim = TRUE, big.mark = ",", 
+                   scientific = FALSE, digits = 1L)
+  
+  ifelse(x < 0, 
+         paste0("\U2212","$", commaz),
+         paste0("$", commaz))
+}
