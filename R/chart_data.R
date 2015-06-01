9d2bd27cd81ab224b67670b9df3716682fcf590a
HughParsonage <hugh.parsonage@gmail.com>
2015-06-01 13:55:45 +1000

    chart_data now actually has an argument...

--- R/chart_data.R
+++ R/chart_data.R
@@ -4,5 +4,5 @@
 
 chart_data <- function(.data){
   current_chunk_label <- opts_current$get(name = "label")
-  readr::write_csv(paste0(current_chunk_label, ".csv"))
+  readr::write_csv(x = .data, path = paste0(current_chunk_label, ".csv"))
 }
