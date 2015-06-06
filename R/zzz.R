5188c6fca514caf89613bf76d542849df9297cf3
HughParsonage <hugh.parsonage@gmail.com>
2015-06-06 18:05:13 +1000

    Made bar plot dark orange

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname = find.package("grattan"), pkgname = "grattan"){
   ggplot2::update_geom_defaults("point", list(colour = Orange))  #but cf. col.3
-  ggplot2::update_geom_defaults("bar", list(fill = Orange, colour = "black", width = 0.7))
+  ggplot2::update_geom_defaults("bar", list(fill = DarkOrange, colour = "black", width = 0.7))
   ggplot2::update_geom_defaults("line", list(fill = Orange, colour = Orange, size = 2))
   if (require(knitr) && require(devEMF)){
 #       my_emf <- function(file, width, height){
