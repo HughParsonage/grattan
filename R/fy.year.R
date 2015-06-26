5529f1555d3d518046987d16a14f33509e47cd5e
HughParsonage <hugh.parsonage@gmail.com>
2015-06-26 16:43:08 +1000

    Fy_year additional features

--- R/fy.year.R
+++ R/fy.year.R
@@ -1,14 +1,23 @@
 #' Convenience function for creating FY
 #' 
 #' @param yr_ending ()
-#' @value the financial year ("YYYY-YY") corresponding to June of \code{yr_ending}
+#' @return the financial year ("YYYY-YY") corresponding to June of \code{yr_ending}
+
+is.fy <- function(fy.yr){
+  !grepl("^.*([12][0-9]{3}).?[0-9]{2}.*$", fy.yr) |
+      # Are the years consecutive?
+      ((as.integer(gsub("^.*([12][0-9]{3}).?[0-9]{2}.*$", "\\1", fy.yr)) + 1) %% 100) == as.numeric(gsub("^.*[12][0-9]{3}.?([0-9]{2}).*$", "\\1", fy.yr))
+}
 
 fy.year <- function(yr_ending){
-  paste0(as.numeric(yr_ending) - 1, "-", substr(yr_ending, 3, 4))
+  paste0(as.integer(yr_ending) - 1, "-", substr(yr_ending, 3, 4))
 }
 
 yr2fy <- function(...) fy.year(...)
 
 fy2yr <- function(fy.yr){
-  1 + as.numeric(gsub("^.*([12][0-9]{3}).[0-9]{2}.*$", "\\1", fy.yr))
+  if(is.fy(fy.yr))
+    stop("Doesn't look like a FY.")
+  else
+    1 + as.integer(gsub("^.*([12][0-9]{3}).?[0-9]{2}.*$", "\\1", fy.yr))
 }
