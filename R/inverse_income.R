3e05c1d2a4133423f154b3c7dbdbe722213eafd5
HughParsonage <hugh.parsonage@gmail.com>
2015-10-17 13:12:46 +1100

    new "uniform" option for zero taxes

--- R/inverse_income.R
+++ R/inverse_income.R
@@ -1,11 +1,13 @@
 #' Inverse income tax functions
 #' 
 #' @param tax the tax payable 
-#' @param zero.tax.income ("maximum", "zero", numeric()) Given that many incomes map to zero taxes, the \code{income_tax} function is not invertible there. As a consequence, the inverse function's value must be specified for tax = 0. "maximum" returns the maximum integer income one can have with a zero tax liability; "zero" returns zero for any tax of zero. The value can also be specified explicitly.
+#' @param zero.tax.income ("maximum", "zero", "uniform", numeric()) Given that many incomes map to zero taxes, the \code{income_tax} function is not invertible there. As a consequence, the inverse function's value must be specified for tax = 0. "maximum" returns the maximum integer income one can have with a zero tax liability; "zero" returns zero for any tax of zero; "uniform" provides a random integer from zero to the maximum income with a zero tax. The value can also be specified explicitly.
 #' @return the taxable income (integer) given the tax payable for the financial year, acurrate to the nearest integer
 #' 
 
-inverse_income <- function(...) inverse_income_lookup3(...)
+inverse_income <- 
+  function(tax, fy.year, zero.tax.income) 
+    inverse_income_lookup3(tax, fy.year = "2012-13", zero.tax.income = c("maximum", "zero", "uniform", numeric(1)))
 
 inverse_income_while <- function(tax, fy.year = "2012-13"){
   income <- 0L
@@ -49,28 +51,35 @@ inverse_income_lookup2 <- function(tax, fy.year = "2012-13", zero.tax = "maximum
 }
 
 inverse_income_lookup3 <- function(tax, fy.year = "2012-13", zero.tax.income = "maximum"){
+  # This function creates a lookup table of incomes taxes 
   zeroes <- tax == 0
-  income.range <- seq(0L, max(ceiling(max(tax) * 3), 100000L), by = 1L)
-  input <- data.table::data.table(taxes = ifelse(zeroes, 1, tax))
+  # We now designate the range of incomes to search over. 
+  # Always check up to $100,000. There, the ratio of income to 
+  # tax is at most 3.79 and decreases thereafter (2012-13).
+  income.range <- seq(0L, max(ceiling(max(tax) * 3.79), 100000L), by = 1L)  
+  input <- data.table::data.table(taxes = ifelse(zeroes, 1, tax))  # ensure a one-to-one relationship
   temp <- data.table::data.table(incomes = income.range)
   temp$taxes <- grattan::income_tax(temp$incomes, fy.year = fy.year)
   data.table::setkey(temp, taxes)
   # tbl <- temp[input, roll=-Inf]  # LOOCF, all taxes are invertible
   tbl <- data.table:::`[.data.table`(temp, input, roll=-Inf)
   
-  # Take care of zeroes
   out <- tbl$incomes
-  if(length(out) == 0)
-    out <- numeric(1)
-  
+  # Take care of zeroes
   if(any(zeroes)){
-    if (zero.tax.income == "maximum"){
-      out[zeroes] <- inverse_income_while(0, fy.year = fy.year) - 1
+    if (zero.tax.income == "zero"){
+      out[zeroes] <- 0L
     } else {
-      if (zero.tax.income == "zero"){
-        out[zeroes] <- 0L
-      } else {
+      if(is.numeric(zero.tax.income)){
         out[zeroes] <- zero.tax.income
+      } else {
+        max.zero.tax.income <- inverse_income_while(0, fy.year = fy.year) - 1
+        if (zero.tax.income == "maximum"){
+          out[zeroes] <- max.zero.tax.income
+        } else {
+          if(zero.tax.income == "uniform")
+            out[zeroes] <- sample(0:max.zero.tax.income, size = sum(zeroes))
+        }
       }
     }
   }
