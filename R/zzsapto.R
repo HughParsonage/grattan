f835d41f410977fb8a41af0b5a7507bb5e9c8c17
HughParsonage <hugh.parsonage@gmail.com>
2015-11-21 16:05:00 +1100

    sapto function
    Expected to be for internal use only

---
+++ R/zzsapto.R
@@ -0,0 +1,32 @@
+#' A function to calculate the Senior Australian and Pensioner Tax Offset
+#' 
+#' @param age (numeric) age in years
+#' @param age_group (fctr) age in 5 year groups
+#' @return the amount that will offset one's estimated tax
+
+
+.sapto <- function(income, age, age_group, is.single = TRUE, fy.year = "2012-13"){
+  if (missing(age) && missing(age_group))
+    stop("At least one of 'age' and 'age_group' must be provided")
+  
+  if (!missing(age) && missing(age_group)){
+    over65 <- age >= 65
+  } 
+  
+  if (missing(age) && !missing(age_group)){
+    over65 <- age_group >= "65 to 69"
+  }
+  
+  if(!missing(age) && !missing(age_group)){
+    stopifnot(all(!is.na(age)), all(!is.na(age_group)))
+    over65 <- age >= 65 | age_group >= "65 to 69"
+  }
+  ifelse(!over65,
+         0,
+         ifelse(is.single,
+                pmin(2230, pmax(0, 2230 - (income - 32279) * 0.125)),
+                pmin(1602, pmax(0, 1602 - (income - 28974) * 0.125))
+                )
+         )
+}
+  
