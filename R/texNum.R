6cd29577c14ca1ac858820c6bbb4aac639433e55
HughParsonage <hugh.parsonage@gmail.com>
2016-01-18 15:16:40 +1100

    Just wrong before.
    The key is lines 29-30. Previous 4.95 billion = 495 billion.

--- R/texNum.R
+++ R/texNum.R
@@ -3,44 +3,45 @@
 #' @param sig.figs (not used)
 #' 
 
-texNum <- function(number, sig.figs = 3L){
+texNum <- function(number, sig.figs = 2L){
   stopifnot(is.numeric(number))
   is.negative <- number < 0
   number <- abs(number)
-  n.digits <- floor(log10(number))
-  
-  if (number == 0)
+  if (number == 0){
     return(0)
-  
-  suffix <- NULL
-  out <- number
-  
-  if (n.digits < sig.figs)
-    return(number)
-  
-  
-  if(n.digits <= 6){
-    prefix <- prettyNum(round(number, sig.figs - n.digits - 1), big.mark = ",")
-  } else
-    prefix <- format(round(number, (1 + n.digits %% 3) - n.digits), scientific = FALSE)
-  
-  
-  if (n.digits > 6){
-    # prefix <- substr(number, 0, 1 + n.digits %% 3)
-    prefix <- substr(number, 0, sig.figs)
-    suffix <- "million"
+  } else {
+    n.digits <- floor(log10(number))
     
-    if (n.digits > 8){
-      suffix <- "billion"
+    
+    
+    suffix <- NULL
+    out <- number
+    
+    if (n.digits < sig.figs)
+      return(number)
+    
+    
+    if (n.digits <= 6){
+      prefix <- prettyNum(round(number, sig.figs - n.digits - 1), big.mark = ",")
+    } 
+    
+    if (n.digits > 6){
+      # Want to show only the number / 10^(multiple of 3) then the suffix multiplier
+      prefix <- signif(number/10 ^ (3 * (n.digits %/% 3)), digits = sig.figs)
+      suffix <- "million"
+      
+      if (n.digits >= 9){
+        suffix <- "billion"
+      }
+      
+      if (n.digits >= 12){
+        suffix <- "trillion"
+      }
+      
     }
-    
-    if (n.digits > 12){
-      suffix <- "trillion"
-    }
-    
+    out <- paste0(prefix, "~", suffix)
+    if(is.negative)
+      out <- paste0("$-$", out)
+    return(out)
   }
-  out <- paste0(prefix, "~", suffix)
-  if(is.negative)
-    out <- paste0("$-$", out)
-  return(out)
 }
