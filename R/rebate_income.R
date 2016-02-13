6333e62e34269c64a70bc8e54b94bbba55094e8c
HughParsonage <hugh.parsonage@gmail.com>
2016-02-13 18:37:26 +1100

    inconseq -- updated roxygen

--- R/rebate_income.R
+++ R/rebate_income.R
@@ -2,7 +2,10 @@
 #' 
 #' @param Taxable_Income the taxable income
 #' @param Rptbl_Empr_spr_cont_amt The reportable employer superannuation contributions amount
-#' @param 
+#' @param All_deductible_super_contr deductible personal superannuation contributions
+#' @param Net_fincl_invstmt_lss_amt Net financial investment loss
+#' @param Net_rent_amt (for Rental deductions)
+#' @param Rep_frng_ben_amt Reportable fringe-benefits
 #' @source \url{https://www.ato.gov.au/Individuals/Tax-return/2015/Tax-return/Tax-offset-questions-T1-T2/Rebate-income-2015/}
 
 rebate_income <- function(Taxable_Income, 
@@ -16,5 +19,6 @@ rebate_income <- function(Taxable_Income,
        Rptbl_Empr_spr_cont_amt, 
        All_deductible_super_contr,
        Net_fincl_invstmt_lss_amt,
+       pmin(Net_rent_amt, 0),
        floor(Rep_frng_ben_amt * 0.535))
 }
