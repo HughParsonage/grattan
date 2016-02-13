678f153d2b80a4f6c6b88d3fa16850b17a7af4a0
HughParsonage <hugh.parsonage@gmail.com>
2016-02-13 17:12:42 +1100

    init commit of rebate income as input for SAPTO

---
+++ R/rebate_income.R
@@ -0,0 +1,20 @@
+#' Rebate income
+#' 
+#' @param Taxable_Income the taxable income
+#' @param Rptbl_Empr_spr_cont_amt The reportable employer superannuation contributions amount
+#' @param 
+#' @source \url{https://www.ato.gov.au/Individuals/Tax-return/2015/Tax-return/Tax-offset-questions-T1-T2/Rebate-income-2015/}
+
+rebate_income <- function(Taxable_Income, 
+                          Rptbl_Empr_spr_cont_amt,
+                          All_deductible_super_contr = 0,
+                          Net_fincl_invstmt_lss_amt, 
+                          Net_rent_amt, 
+                          Rep_frng_ben_amt){
+  .add <- function(x) Reduce("+", x)
+  .add(Taxable_Income,
+       Rptbl_Empr_spr_cont_amt, 
+       All_deductible_super_contr,
+       Net_fincl_invstmt_lss_amt,
+       floor(Rep_frng_ben_amt * 0.535))
+}
