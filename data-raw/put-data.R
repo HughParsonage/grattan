b8a7fff1b2a544203da96d633fbe53c57da953f1
HughParsonage <hugh.parsonage@gmail.com>
2016-02-13 18:35:18 +1100

    include sapto

--- data-raw/put-data.R
+++ data-raw/put-data.R
@@ -6,4 +6,12 @@
   data.table %>%
   setkey(fy_year)
 
-devtools::use_data(.lito_tbl, tax_tbl, .medicare.tbl.indiv, internal = TRUE, overwrite = TRUE)
+.sapto_tbl <- 
+  readxl::read_excel("./data/SAPTO-rates.xlsx", sheet = 1) %>%
+  data.table %>%
+  setkey(fy_year, family_status, rebate_income)
+
+devtools::use_data(.lito_tbl, tax_tbl, 
+                   .medicare.tbl.indiv, 
+                   .sapto_tbl,
+                   internal = TRUE, overwrite = TRUE)
