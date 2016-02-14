2531cfb7a6436cb81215aa46cb6a183d9f664cf9
HughParsonage <hugh.parsonage@gmail.com>
2016-02-14 21:59:53 +1100

    wage_inflator vectorized and returns NA if the dates are not found, rather than erroring

--- R/wage_inflator.R
+++ R/wage_inflator.R
@@ -1,7 +1,7 @@
 #' a wage inflator
 #' 
 #' @param wage the amount to be inflated (1 by default)
-#' @param from_fy a character string of the form "2012-13" representing the FY ending that the wage index is to be taken (i.e. Q4 in that year). FY year must be 1996-97 or later.
+#' @param from_fy a character vector of the form "2012-13" representing the FY ending that the wage index is to be taken (i.e. Q4 in that year). FY year must be 1996-97 or later.
 #' @param to_fy the FY ending that the wage index is to be taken
 #' @return the wage inflation between the two years
 
@@ -16,20 +16,28 @@ wage_inflator <- function(wage = 1, from_fy, to_fy){
   warning("Using hardcoded wage data")
   }
   
-  from_fy_year <- 1 + as.numeric(gsub("^.*([12][0-9]{3}).*$", "\\1", from_fy)) 
-  from_fy_as_quarter <- paste0(from_fy_year, "-", "Q2")
+  wage.indices <- 
+    data.table::as.data.table(wages) %>%
+    dplyr::filter(grepl("Q1", obsTime)) %>%
+    dplyr::mutate(fy_year = yr2fy(sub("-Q1", "", obsTime, fixed = TRUE)))
   
-  if(!(from_fy_as_quarter %in% wages$obsTime))
-    stop("from_fy not in ABS data")
+  input <-
+    data.table::data.table(wage = wage,
+                           from_fy = from_fy,
+                           to_fy = to_fy)
   
-  to_fy_year <- 1 + as.numeric(gsub("^.*([12][0-9]{3}).*$", "\\1", to_fy)) 
-  to_fy_as_quarter <- paste0(to_fy_year, "-", "Q2")
+  output <- 
+    input %>%
+    data.table:::merge.data.table(wage.indices, by.x = "from_fy", by.y = "fy_year", sort = FALSE,
+                                  all.x = TRUE) %>%
+    dplyr::rename(from_index = obsValue) %>%
+    data.table:::merge.data.table(wage.indices, by.x = "to_fy", by.y = "fy_year", sort = FALSE, 
+                                  all.x = TRUE) %>%
+    dplyr::rename(to_index = obsValue) %>%
+    dplyr::mutate(out = wage * (to_index/from_index)) 
   
-  if(!(to_fy_as_quarter %in% wages$obsTime))
-    stop("to_fy not in ABS data")
-  
-  wage * 
-    wages[wages$obsTime == to_fy_as_quarter, ]$obsValue / 
-      wages[wages$obsTime == from_fy_as_quarter, ]$obsValue
-
+#   wage * 
+#     wages[wages$obsTime == to_fy_as_quarter, ]$obsValue / 
+#       wages[wages$obsTime == from_fy_as_quarter, ]$obsValue
+  output$out
 }
