ac70a60ae8f50df0bf5133cc22afcfdca4e626c7
HughParsonage <hugh.parsonage@gmail.com>
2016-02-20 12:25:44 +1100

    Vectorize fy checking (and updated error messages accordingly)

--- R/fy.year.R
+++ R/fy.year.R
@@ -16,10 +16,18 @@ fy.year <- function(yr_ending){
 yr2fy <- function(...) fy.year(...)
 
 fy2yr <- function(fy.yr){
-  if(!is.fy(fy.yr))
-    stop("Doesn't look like a FY.")
-  else
+  if(any(!is.fy(fy.yr))){
+    stop("fy.yr contains non-FYs")
+  } else  {
     1 + as.integer(gsub("^.*([12][0-9]{3}).?[0-9]{2}.*$", "\\1", fy.yr))
+  }
 }
 
-fy2date <- function(x){if(!is.fy(x)){stop("Not an FY")} else {date <- paste0(as.numeric(gsub("^([1-9][0-9]{3}).*", "\\1", x)) + 1, "-06-30"); as.Date(date)}}
+fy2date <- function(x){
+  if(!any(is.fy(x))){
+    stop("fy.yr contains non-FYs")
+  } else {
+    date <- paste0(as.numeric(gsub("^([1-9][0-9]{3}).*", "\\1", x)) + 1, "-06-30") 
+    as.Date(date)
+  }
+}
