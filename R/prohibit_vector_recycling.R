32e9683cf32388a048b57d1a93dc24391956f767
HughParsonage <hugh.parsonage@gmail.com>
2016-02-20 13:28:02 +1100

    Added vector recycling test function

---
+++ R/prohibit_vector_recycling.R
@@ -0,0 +1,23 @@
+#' Prohibit vector recyling
+#' 
+#' Tests (harshly) whether the vectors can be recycled safely.
+#' 
+#' @param ... A list of vectors
+#' @return An error message if the vectors are of different length (unless that length is 1).
+#' @examples 
+#' # Returns nothing because they are of the same length
+#' prohibit_vector_recycling(c(2, 2), c(2, 2))
+#' # Returns nothing also, because the only different length is 1
+#' prohibit_vector_recycling(c(2, 2), 1)
+#' # Returns an error:
+#' prohibit_vector_recycling(c(2, 2), 1, c(3, 3, 3))
+#' @source \url{http://stackoverflow.com/a/9335687/1664978}
+
+prohibit_vector_recycling <- function(...){
+  # http://stackoverflow.com/a/9335687/1664978
+  lengths <- sapply(list(...), length)
+  max.length <- max(lengths)
+  if (any((lengths != 1L & lengths != max.length))){
+    stop("Inputs must be of equal length, or length 1")
+  }
+}
