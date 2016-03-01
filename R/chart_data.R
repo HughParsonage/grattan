8198ef7ea91cc66244eb79f9d4d9fb3f2c5fabe6
HughParsonage <hugh.parsonage@gmail.com>
2016-03-02 09:06:24 +1100

    chart_data

--- R/chart_data.R
+++ R/chart_data.R
@@ -1,8 +1,19 @@
 #' chart_data
 #' 
 #' convenience function for knitr chunks
+#' 
+#' @param .data A data frame suitable for \code{readr::write_csv}.
+#' @return If knitting, writes a csv of its named as the current chunk label. Otherwise, returns \code{.data} invisibly.
+#' @usage Use this in a knitr chunk that produces a chart to get the chart data 
 
 chart_data <- function(.data){
-  current_chunk_label <- opts_current$get(name = "label")
-  readr::write_csv(x = .data, path = paste0("./figure/", current_chunk_label, ".csv"))
+  current_chunk_label <- knitr::opts_current$get(name = "label")
+  if(!is.null(current_chunk_label)){  # i.e. we are knitting
+    if(!exists(atlas)){
+      atlas <- knitr::opts_chunk$get(name = "fig.path")
+    }
+    readr::write_csv(x = .data, path = file.path(atlas, paste0(current_chunk_label, ".csv")))
+  } else {
+    invisible(.data)
+  }
 }
