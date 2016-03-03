349b567ec1b19ad2b23da1f40d04571ec8e33457
HughParsonage <hugh.parsonage@gmail.com>
2016-03-03 11:53:52 +1100

    Don't run code that is designed to error

--- R/prohibit_vector_recycling.R
+++ R/prohibit_vector_recycling.R
@@ -5,12 +5,14 @@
 #' @param ... A list of vectors
 #' @return An error message if the vectors are of different length (unless the alternative length is 1).
 #' @examples 
+#' \dontrun{
 #' # Returns nothing because they are of the same length
 #' prohibit_vector_recycling(c(2, 2), c(2, 2))
 #' # Returns nothing also, because the only different length is 1
 #' prohibit_vector_recycling(c(2, 2), 1)
 #' # Returns an error:
 #' prohibit_vector_recycling(c(2, 2), 1, c(3, 3, 3))
+#' }
 #' @source \url{http://stackoverflow.com/a/9335687/1664978}
 
 prohibit_vector_recycling <- function(...){
