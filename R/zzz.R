5e71b6b6d157ba7351e8413338556003b48c65fd
HughParsonage <hugh.parsonage@gmail.com>
2016-03-14 18:17:10 +1100

    width causing issues so keep in line with ggplot2 for now

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname = find.package("grattan"), pkgname = "grattan"){
   ggplot2::update_geom_defaults("point", list(colour = Orange))  #but cf. col.3
-  ggplot2::update_geom_defaults("bar", list(fill = DarkOrange, colour = "black", width = 0.7))
+  ggplot2::update_geom_defaults("bar", list(fill = DarkOrange, colour = "black"))
   ggplot2::update_geom_defaults("line", list(fill = Orange, colour = Orange, size = 2))
 }
