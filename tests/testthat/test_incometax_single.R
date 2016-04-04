fa358566ecafcef0c257319f0138fc8590522f5b
HughParsonage <hugh.parsonage@gmail.com>
2016-04-04 12:19:56 +1000

    200k income

--- tests/testthat/test_incometax_single.R
+++ tests/testthat/test_incometax_single.R
@@ -10,7 +10,7 @@ test_that("income_tax returns known results",{
   expect_equal(grattan:::.income_tax(50e3, fy.year = "2012-13"), 8297)
   expect_equal(grattan:::.income_tax(60e3, fy.year = "2012-13"), 11847)
   expect_equal(grattan:::.income_tax(70e3, fy.year = "2012-13"), 15347)
-  
+  expect_equal(grattan:::.income_tax(200e3, fy.year = "2012-13"), 66547)
 })
 
 test_that("income_tax is not NA for years)", {
