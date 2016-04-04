218edab94c60a88342e0d208c08b8a9a84a3c505
HughParsonage <hugh.parsonage@gmail.com>
2016-04-04 12:15:49 +1000

    cosmetic

--- tests/testthat/test_incometax_single.R
+++ tests/testthat/test_incometax_single.R
@@ -2,7 +2,6 @@
 context("Individual income tax")
 
 test_that("income_tax returns known results",{
-
   
   expect_equal(income_tax(50e3, fy.year = "2012-13"), 8297)
   expect_equal(income_tax(60e3, fy.year = "2012-13"), 11847)
@@ -11,7 +10,6 @@ test_that("income_tax returns known results",{
   expect_equal(grattan:::.income_tax(50e3, fy.year = "2012-13"), 8297)
   expect_equal(grattan:::.income_tax(60e3, fy.year = "2012-13"), 11847)
   expect_equal(grattan:::.income_tax(70e3, fy.year = "2012-13"), 15347)
-  
   
 })
 
