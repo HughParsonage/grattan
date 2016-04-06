5755b24b1b023e85e6ca567e3548ebfff876f709
HughParsonage <hugh.parsonage@gmail.com>
2016-04-06 17:48:17 +1000

    Avoid R CMD CHECK note by abandoning the pipe entirely.

--- R/cgt_inflator.R
+++ R/cgt_inflator.R
@@ -6,14 +6,10 @@
 #' @param from_fy The financial year of the current value of CGT.
 #' @param to_fy The financial year for which CGT is intended to be inflated.
 #' @return An estimate of the CGT payable in \code{to_fy}, inflated by relative tax expenditures. 
+#' @importFrom magrittr %T>%
 
 CGT_inflator <- function(cgt = 1, from_fy, to_fy){
-  cgt_expenditures %T>%
-  {
-    stopifnot(length(from_fy) == 1L, length(to_fy) == 1L, from_fy %in% .$FY, to_fy %in% .$FY) 
-  } %>%
-  {
-    .$CGT_discount_for_individuals_and_trusts_millions[which(.$FY == to_fy)] / 
-      .$CGT_discount_for_individuals_and_trusts_millions[which(.$FY == from_fy)]
-  }
+  stopifnot(length(from_fy) == 1L, length(to_fy) == 1L, from_fy %in% cgt_expenditures$FY, to_fy %in% cgt_expenditures$FY)
+  cgt_expenditures$CGT_discount_for_individuals_and_trusts_millions[which(cgt_expenditures$FY == to_fy)] / 
+    cgt_expenditures$CGT_discount_for_individuals_and_trusts_millions[which(cgt_expenditures$FY == from_fy)]
 }
