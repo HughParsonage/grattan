3fd1a4879e1eaf07d5eafdfa4f1ed872b3eb6571
HughParsonage <hugh.parsonage@gmail.com>
2016-04-06 10:52:50 +1000

    texNum pre.phrase

--- R/texNum.R
+++ R/texNum.R
@@ -1,10 +1,15 @@
 #' Convert number to English prose
 #' 
-#' @param sig.figs (not used)
+#' @param A single numeric vector.
+#' @param sig.figs Significant figures to be displayed
+#' @param dollar Logical, should a LaTeX dollar sign (\code{\\$}) be prefixed.
+#' @param pre.phrase A length 2 character vector to insert a phrase before the number. The first element is the phrase to be used if the output rounds up before printing (i.e. the original number is smaller than the number printed); the second is the phrase to be used if the number rounds down. The default is \code{NULL}, i.e. no phrase inserted. If either string has a trailing tilde (e.g. \code{"almost~"}), the tilde separates the number and the phrase (as a LaTeX control sequence).
+#' @return A character string representing the number as appropriate for English prose.
 #' 
 
-texNum <- function(number, sig.figs = 3L, dollar = FALSE){
-  stopifnot(is.numeric(number))
+texNum <- function(number, sig.figs = 3L, dollar = FALSE, pre.phrase = NULL){
+  orig.number <- number
+  stopifnot(is.numeric(number), length(number) == 1L)
   is.negative <- number < 0
   number <- abs(number)
   if (number == 0){
@@ -13,6 +18,7 @@ texNum <- function(number, sig.figs = 3L, dollar = FALSE){
     n.digits <- ceiling(log10(number))
     
     suffix <- NULL
+    suffix_val <- 1
     out <- number
     
     if (n.digits < sig.figs){
@@ -25,20 +31,25 @@ texNum <- function(number, sig.figs = 3L, dollar = FALSE){
     
     
     if (n.digits <= 6){
-      prefix <- prettyNum(round(number, sig.figs - n.digits - 1), big.mark = ",", scientific = FALSE)
+      prefix_val <- round(number, sig.figs - n.digits - 1)
+      prefix <- prettyNum(prefix_val, big.mark = ",", scientific = FALSE)
     } 
     
     if (n.digits > 6){
       # Want to show only the number / 10^(multiple of 3) then the suffix multiplier
-      prefix <- signif(number/10 ^ (3 * (n.digits %/% 3)), digits = sig.figs)
+      prefix_val <- signif(number/10 ^ (3 * (n.digits %/% 3)), digits = sig.figs)
+      prefix <- prefix_val
       suffix <- "~million"
+      suffix_val <- 10^6
       
       if (n.digits >= 9){
         suffix <- "~billion"
+        suffix_val <- 10^9
       }
       
       if (n.digits >= 12){
         suffix <- "~trillion"
+        suffix_val <- 10^12
       }
       
       if (n.digits > 15){
@@ -55,6 +66,18 @@ texNum <- function(number, sig.figs = 3L, dollar = FALSE){
     
     if(is.negative){
       out <- paste0("$-$", out)
+    }
+    # is the displayed number larger than the original?
+    if (!is.null(pre.phrase)){
+      out_larger <- prefix_val * suffix_val > orig.number
+      out <- 
+        if (out_larger) {
+          paste(pre.phrase[1], out, sep = if(grepl("~$", pre.phrase[1])) "" else " ")
+        } else {
+          if (!isTRUE(all.equal(prefix_val * suffix_val , orig.number)))
+            paste(pre.phrase[2], out, sep = if(grepl("~$", pre.phrase[2])) "" else " ")
+        }
+      
     }
     return(out)
   }
