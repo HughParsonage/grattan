921c3f334409dd5ef39c54a7e7484dd1a98798a8
HughParsonage <hugh.parsonage@gmail.com>
2016-04-07 18:00:29 +1000

    Forgot to export gpalx

--- R/gpalx.R
+++ R/gpalx.R
@@ -4,8 +4,8 @@
 #' @param n a positive integer
 #' @details This function interpolates the original Grattan palette for a large (possibly infinite) number of factors.  
 #' @return A vector of n HTML colours.
+#' @export
 
 gpalx <- function(n){
-  stopifnot(requireNamespace("grattan", quietly = TRUE))
   grDevices::colorRampPalette(colors = grattan::pal.6)(n)
 }
