72eb15ecfc43111472327f141d12504501a3a02c
HughParsonage <hugh.parsonage@gmail.com>
2016-07-07 14:59:47 +1000

    Preparation for new theme, specialized for LaTeX reports

--- R/theme_hugh.R
+++ R/theme_hugh.R
@@ -10,54 +10,119 @@
 #' @importFrom ggplot2 element_blank
 #' @export
 
-theme_hugh <- function(base_size = 23, base_family = "") {
-  ggplot2::theme_classic(base_size = base_size, 
-                         base_family = base_family) %+replace%
-    ggplot2::theme(
-      
-      # Base elements which are not used directly but inherited by others
-      line =              element_line(colour = '#DADADA', size = 0.5, 
-                                       linetype = 1, lineend = "butt"),
-      rect =              element_rect(fill = "#F0F0F0", colour = "#F0F0F0", 
-                                       size = 0.5, linetype = 1),
-      text =              element_text(family = base_family, face = "plain",
-                                       colour = "#000000", size = base_size,
-                                       hjust = 0.5, vjust = 0.5, angle = 0, 
-                                       lineheight = 0.9, 
-                                       margin = ggplot2::margin(), debug = FALSE),
-      plot.title = element_text(hjust = 0, vjust = 1.5),
-      # Puts x lab in the centre, boldface
-      axis.title.x =      element_text(hjust = 0.5, vjust = 0, 
-                                       face = "bold",
-                                       margin = ggplot2::margin(t = 11.5, b = 11.5)),
-      axis.title.y =      element_blank(),
-      axis.text =         element_text(),
-      axis.line =         element_line(color = "black", size = 1),
-      axis.line.x  = element_line(color = "black", size = 1), 
-      axis.line.y  = element_line(color = "black", size = 1),
-      axis.ticks.length = grid::unit(0.3, "lines"),
-      
-      # Modified inheritance structure of line element
-      axis.ticks =        element_line(color = "black"),
-      panel.grid.major =  element_line(),
-      panel.grid.major.x = element_blank(),
-      panel.grid.major.y = element_line(),
-      panel.grid.minor =  element_blank(),
-      
-      # Modified inheritance structure of rect element
-      plot.background =   element_blank(),
-      plot.margin = grid::unit(c(0.7,0.1,0.5,0.00), "lines"),
-      panel.background =  element_blank(),
-      legend.key =        element_rect(fill='white', size = 0.5),
-      legend.key.size = grid:: unit(1.5, "lines"), 
-      legend.text = element_text(), 
-      
-      # Modifiying legend.position
-      legend.position = 'none',
-      legend.background = element_rect(fill = NA, colour = NA),
-      legend.title = element_text(face = "bold"),
-      strip.background = element_rect(fill = "white"),
-      
-      complete = TRUE
-    )
+if (isTRUE(getOption("grattan.bigplot"))) {
+  theme_hugh <- function(base_size = 23, base_family = "") {
+    ggplot2::theme_classic(base_size = base_size, 
+                           base_family = base_family) %+replace%
+      ggplot2::theme(
+        
+        # Base elements which are not used directly but inherited by others
+        line =              element_line(colour = '#DADADA', size = 0.5, 
+                                         linetype = 1, lineend = "butt"),
+        rect =              element_rect(fill = "#F0F0F0", colour = "#F0F0F0", 
+                                         size = 0.5, linetype = 1),
+        text =              element_text(family = base_family, face = "plain",
+                                         colour = "#000000", size = base_size,
+                                         hjust = 0.5, vjust = 0.5, angle = 0, 
+                                         lineheight = 0.9, 
+                                         margin = ggplot2::margin(), debug = FALSE),
+        plot.title = element_text(hjust = 0, vjust = 1.5),
+        # Puts x lab in the centre, boldface
+        axis.title.x =      element_text(hjust = 0.5, vjust = 0, 
+                                         face = "bold",
+                                         margin = ggplot2::margin(t = 11.5, b = 11.5)),
+        axis.title.y =      element_blank(),
+        axis.text =         element_text(),
+        axis.line =         element_line(color = "black", size = 1),
+        axis.line.x  = element_line(color = "black", size = 1), 
+        axis.line.y  = element_line(color = "black", size = 1),
+        axis.ticks.length = grid::unit(0.3, "lines"),
+        
+        # Modified inheritance structure of line element
+        axis.ticks =        element_line(color = "black"),
+        panel.grid.major =  element_line(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(),
+        panel.grid.minor =  element_blank(),
+        
+        # Modified inheritance structure of rect element
+        plot.background =   element_blank(),
+        plot.margin = grid::unit(c(0.7,0.1,0.5,0.00), "lines"),
+        panel.background =  element_blank(),
+        legend.key =        element_rect(fill='white', size = 0.5),
+        legend.key.size = grid:: unit(1.5, "lines"), 
+        legend.text = element_text(), 
+        
+        # Modifiying legend.position
+        legend.position = 'none',
+        legend.background = element_rect(fill = NA, colour = NA),
+        legend.title = element_text(face = "bold"),
+        strip.background = element_rect(fill = "white"),
+        
+        complete = TRUE
+      )
+  }
+  
+} else {
+  theme_hugh <- function(base_size = 10, base_family = ""){
+      ggplot2::theme(
+        line = element_line(colour = '#DADADA', 
+                            size = 0.5, 
+                            linetype = 1, 
+                            lineend = "butt"),
+        rect = element_rect(fill = "#F0F0F0", 
+                            colour = "#F0F0F0", 
+                            size = 0.5, 
+                            linetype = 1),
+        text = element_text(family = base_family, 
+                            face = "plain",
+                            colour = "#000000", 
+                            size = base_size,
+                            hjust = 0.5, 
+                            vjust = 0.5, 
+                            angle = 0, 
+                            lineheight = 0.9, 
+                            margin = ggplot2::margin(), 
+                            debug = FALSE),
+        title = element_text(),
+        aspect.ratio = 4.472222/2.8444, 
+        # Puts x lab in the centre, boldface
+        axis.title.x = element_text(hjust = 0.5, vjust = 0, 
+                                    face = "bold",
+                                    margin = ggplot2::margin(t = 5, b = 5, unit = "pt")),
+        # y-axis handled as a caption
+        axis.title.y = element_blank(),
+        axis.text = element_text(),
+        axis.line = element_line(color = "black", size = 1),
+        axis.line.x = element_line(color = "black", size = 1), 
+        axis.line.y = element_line(color = "black", size = 1),
+        axis.ticks.length = grid::unit(0.3, "lines"),
+        
+        # Modified inheritance structure of line element
+        axis.ticks = element_line(color = "black"),
+        panel.grid.major =  element_line(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(),
+        panel.grid.minor =  element_blank(),
+        
+        # Modified inheritance structure of rect element
+        plot.background = element_blank(),
+        plot.margin = grid::unit(c(0.5, 0.1, 0.5, 0.00), "lines"),
+        panel.background = element_blank(),
+        legend.key = element_rect(fill='white', size = 0.5),
+        legend.key.size = grid:: unit(1.5, "lines"), 
+        legend.text = element_text(), 
+        
+        # Modifiying legend.position
+        legend.position = 'none',
+        legend.background = element_rect(fill = "white", colour = NA),
+        legend.justification = c(1, 0),
+        legend.title = element_text(face = "bold", margin = ggplot2::margin(t = 0, b = 11)),
+        strip.background = element_rect(fill = "white", color = NA),
+        strip.text = element_text(face = "bold"),
+        
+        complete = TRUE
+      )
+  }
 }
+  
