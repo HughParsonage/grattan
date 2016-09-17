ecabbc3bc666e2b1f96f8703e9d7427b73c739e0
HughParsonage <hugh.parsonage@gmail.com>
2016-09-17 12:44:58 +1000

    PTO-SATO handling

--- R/medicare_levy.R
+++ R/medicare_levy.R
@@ -5,11 +5,16 @@
 #' @param income The taxable income. A vector of numeric values.
 #' @param fy.year The financial year. A character vector satisfying \code{is.fy}.
 #' @param Spouse_income The spouse's adjusted income.
-#' @param sapto.eligible (logical) Is the taxpayer eligible for SAPTO?
+#' @param sapto.eligible (logical) Is the taxpayer eligible for SAPTO? See Details.
+#' @param sato Is the taxpayer eligible for the Senior Australians Tax Offset?
+#' @param pto Is the taxpayer eligible for the Pensions Tax Offset? \code{sato} and \code{pto} must be 
 #' @param family_status What is the taxpayer's family status: family or individual?
 #' @param n_dependants Number of children dependant on the taxpayer.
 #' @return The medicare levy payable for that taxpayer.
-#' @details The actual medicare levy payable for such a taxpayer in that financial year.
+#' @details The Seniors and Pensioners Tax Offset was formed in 2012-13 as an amalgam of the Senior Australians Tax Offset and the Pensions Tax Offset. 
+#' Medicare rates before 2012-13 were different based on these offsets. 
+#' If \code{sato} and \code{pto} are \code{NULL}, \code{sapto.eligible} stands for eligibility for the \code{sato} and not \code{pto}.
+#' If \code{sato} or \code{pto} are not \code{NULL} for such years, 
 #' @export
 #' 
 
@@ -17,6 +22,8 @@ medicare_levy <- function(income,
                           fy.year = "2013-14",
                           Spouse_income = 0,
                           sapto.eligible = FALSE,
+                          sato = NULL,
+                          pto = NULL,
                           family_status = "individual", 
                           n_dependants = 0){
   stopifnot(all(is.fy(fy.year)), all(family_status %in% c("family", "individual")))
@@ -25,10 +32,37 @@ medicare_levy <- function(income,
     stop("If Spouse_income is nonzero, family_status cannot be 'individual'.")
   }
   
+  # If sapto.eligible = TRUE, sato = TRUE, pto = FALSE
+  stopifnot(is.null(sato) || is.logical(sato), 
+            is.null(pto)  || is.logical(pto))
+  
+  # Allow a join on a complete sato, pto, sapto key
+  # To do this we need to make sato = sapto.eligible
+  # and pto = !sato when required. 
+  if (is.null(sato) && is.null(pto)){
+    sato <- sapto.eligible
+    pto <- sapto.eligible & !sato
+  } else {
+    if (is.null(sato)){
+      sato <- !pto
+    } else {
+      if (is.null(pto)){
+        pto <- !sato
+      }
+    }
+    if (any(sato & pto)) {
+      stop("pto and sato must not both be TRUE")
+    }
+  }
+  
+  
+  
   data.table(income = income, 
              Spouse_income = Spouse_income,
              fy_year = fy.year,
              sapto = sapto.eligible, 
+             sato = sato, 
+             pto = pto,
              family_status = family_status) %>%
     # Assume spouse income is included irrespective of Partner_status
     # This appears to be the correct treatment (e.g. if the Partner dies 
@@ -39,7 +73,7 @@ medicare_levy <- function(income,
     # Enhancement: family taxable income should exclude super lump sums.
     .[ ,family_income := income + Spouse_income ] %>%
     merge(medicare_tbl, 
-          by = c("fy_year", "sapto"),
+          by = c("fy_year", "sapto", "sato", "pto"),
           sort = FALSE, 
           all.x = TRUE) %>%
     # Levy in the case of small incomes (s.7 of Act)
