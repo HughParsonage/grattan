a1020a79e6ff056094868b8c5fd2e47ab523b032
HughParsonage <hugh.parsonage@gmail.com>
2016-09-18 19:57:58 +1000

    Increase detail of `wage_inflator` forecast training data

--- R/wage_inflator.R
+++ R/wage_inflator.R
@@ -1,15 +1,19 @@
 #' @title A wage inflator
 #' @description Predicts what a wage could expect to grow to, between two financial years.
 #' 
-#' @param wage the amount to be inflated (1 by default)
-#' @param from_fy a character vector of the form "2012-13" representing the FY ending that the wage index is to be taken (i.e. Q4 in that year). FY year must be 1996-97 or later.
-#' @param to_fy the FY ending that the wage index is to be taken
+#' @param wage The amount to be inflated (1 by default).
+#' @param from_fy A character vector of the form "2012-13" representing the FY ending that the wage index is to be taken (i.e. Q4 in that year). FY year must be 1996-97 or later.
+#' @param to_fy The FY ending that the wage index is to be taken.
 #' @param useABSConnection Should the function connect with ABS.Stat via an SDMX connection? By default set to \code{FALSE} in which case a pre-prepared index table is used. This is much faster and more reliable (in terms of errors), though of course relies on the package maintainer to keep the tables up-to-date.
-#' @param allow.projection If set to \code{TRUE} the \code{forecast} package is used to project forward. If set to \code{FALSE}, any dates outside the range will return \code{NA}.
-#' @return the wage inflation between the two years
+#' @param allow.projection If set to \code{TRUE} the \code{forecast} package is used to project forward, if required. 
+#' @param forecast.series Whether to use the forecast mean, or the upper or lower boundaries of the prediction intervals.
+#' @param forecast.level The prediction interval to be used if \code{forecast.series} is \code{upper} or \code{lower}. 
+#' @return The wage inflation between the two years.
 #' @export
 
-wage_inflator <- function(wage = 1, from_fy, to_fy, useABSConnection = FALSE, allow.projection = TRUE){
+wage_inflator <- function(wage = 1, from_fy, to_fy, useABSConnection = FALSE, allow.projection = TRUE, 
+                          forecast.series = c("mean", "upper", "lower"), 
+                          forecast.level = 95){
   
   # CRAN
   obsTime <- NULL; obsValue <- NULL; to_index <- NULL; from_index <- NULL
@@ -31,56 +35,90 @@ wage_inflator <- function(wage = 1, from_fy, to_fy, useABSConnection = FALSE, al
   }
   
   obsDate <- NULL
-  wage.indices[, obsDate := as.Date(sprintf("%s-01", obsTime))]
-  last.date.in.series <- last(wage.indices[["obsDate"]])
-  last.full.fy.in.series <- 
-    wage.indices %>%
-    dplyr::filter(month(obsDate) == 6L) %>%
-    .[["obsDate"]] %>%
-    last %>%
-    date2fy
+  wage.indices[, obsYear := as.integer(sub("([12][0-9]{3}).Q([1-4])", "\\1", obsTime, perl = TRUE))]
+  wage.indices[, obsQtr  := as.integer(sub("([12][0-9]{3}).Q([1-4])", "\\2", obsTime, perl = TRUE))]
   
-  if (!allow.projection && any(to_fy > last.full.fy.in.series)){
+  last.full.yr.in.series <- 
+    wage.indices %>%
+    dplyr::filter(obsQtr == 2L) %>%
+    .[["obsYear"]] %>%
+    last 
+  
+  last.quarter.in.series <- 
+    wage.indices %>%
+    .[["obsQtr"]] %>%
+    last 
+  
+  if (!allow.projection && to_fy > yr2fy(last.full.yr.in.series)){
     stop("Not all elements of to_fy are in wage index data.")
   }
   # else allow NAs to propagate
   
   # Use forecast::forecast to inflate forward
-  if(allow.projection && isTRUE(!all(to_fy %in% wage.indices$fy_year))){
-    # Number of years beyond the data our forecast must reach
-    to_date <- fy2date(to_fy)
-    months.ahead <- 
-      12L * (year(to_date) - year(last.date.in.series)) + month(to_date) - month(last.date.in.series)
+  if(allow.projection && to_fy > yr2fy(last.full.yr.in.series)){
+    # Number of quarters beyond the data our forecast must reach
+    quarters.ahead <- 
+      4L * (fy2yr(to_fy) - last.full.yr.in.series) + 2L - last.quarter.in.series
     
+    forecast.series <- match.arg(forecast.series)
+    switch(forecast.series, 
+           "mean" = {
+             forecasts <- 
+               forecast::forecast(wage.indices[["obsValue"]], 
+                                  h = quarters.ahead, 
+                                  level = forecast.level) %>%
+               magrittr::use_series("mean") %>%
+               as.numeric 
+           }, 
+           "upper" = {
+             forecasts <- 
+               forecast::forecast(wage.indices[["obsValue"]], 
+                                  h = quarters.ahead, 
+                                  level = forecast.level) %>%
+               magrittr::use_series("upper") %>%
+               as.numeric 
+           }, 
+           "lower" = {
+             forecasts <- 
+               forecast::forecast(wage.indices[["obsValue"]], 
+                                  h = quarters.ahead, 
+                                  level = forecast.level) %>%
+               magrittr::use_series("lower") %>%
+               as.numeric
+           })
     
-    wage_index_forecast <- wage.indices %$% forecast::forecast(obsValue, h = years.beyond) %$% as.numeric(mean)
+    obsQtr <- obsYear <- NULL
     wage.indices.new <- 
-      data.table::data.table(fy_year = yr2fy(seq(max(fy2yr(wage.indices$fy_year)) + 1,
-                                                     max(fy2yr(to_fy)),
-                                                     by = 1L)),
-                             obsValue = wage_index_forecast)
+      data.table(obsQtr   = (seq(last(wage.indices[["obsQtr"]]) + 1, length.out = quarters.ahead, by = 1) %% 4) + 1, 
+                 obsValue = forecasts) %>%
+      .[, obsYear := last(wage.indices[["obsYear"]]) + cumsum(obsQtr == 1L)]
+      
     wage.indices <- data.table::rbindlist(list(wage.indices, wage.indices.new), use.names = TRUE, fill = TRUE)
   }
+  
+  wage.indices %<>%
+    .[obsQtr == 2L] %>%
+    .[, fy_year := yr2fy(obsYear)]
   
   input <-
     data.table::data.table(wage = wage,
                            from_fy = from_fy,
                            to_fy = to_fy)
   
-  
-  
   output <- 
     input %>%
-    merge(wage.indices, by.x = "from_fy", by.y = "fy_year", sort = FALSE, all.x = TRUE) %>%
-    dplyr::rename(from_index = obsValue) %>%
-    merge(wage.indices, by.x = "to_fy", by.y = "fy_year", sort = FALSE, all.x = TRUE) %>%
-    dplyr::rename(to_index = obsValue) %>%
-    dplyr::mutate(out = wage * (to_index/from_index)) 
+    merge(wage.indices, by.x = "from_fy", by.y = "fy_year", sort = FALSE,
+          all.x = TRUE) %>%
+    setnames("obsValue", "from_index") %>%
+    merge(wage.indices, by.x = "to_fy", by.y = "fy_year", sort = FALSE, 
+          all.x = TRUE) %>%
+    setnames("obsValue", "to_index") %>%
+    .[, out := wage * (to_index/from_index)]
   
   
   
 #   wage * 
 #     wages[wages$obsTime == to_fy_as_quarter, ]$obsValue / 
 #       wages[wages$obsTime == from_fy_as_quarter, ]$obsValue
-  output$out
+  output[["out"]]
 }
