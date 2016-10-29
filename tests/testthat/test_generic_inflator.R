66e50e4babfcf57699613037c474f138b9b17e22
HughParsonage <hugh.parsonage@gmail.com>
2016-10-29 17:56:04 +1100

    Increase coverage

--- tests/testthat/test_generic_inflator.R
+++ tests/testthat/test_generic_inflator.R
@@ -4,3 +4,11 @@ test_that("generic inflator doesn't fail!", {
   expect_is(generic_inflator(vars = "Sw_amt", h = 0L), "data.frame")
   expect_is(generic_inflator(vars = "Sw_amt", h = 1L), "data.frame")
 })
+  
+test_that("generic inflator gives higher/lower for upper/lower", {
+  expect_gte(generic_inflator(vars = "Sw_amt", h = 1L, estimator = "mean"), 
+             generic_inflator(vars = "Sw_amt", h = 1L, estimator = "lower"))
+  
+  expect_lte(generic_inflator(vars = "Sw_amt", h = 1L, estimator = "mean"), 
+             generic_inflator(vars = "Sw_amt", h = 1L, estimator = "upper"))
+})
