a39376f32732622161d40da060a63eb99ddc97eb
HughParsonage <hugh.parsonage@gmail.com>
2016-10-30 23:58:40 +1100

    Tests for aus pop qtr and wage_inflator

--- tests/testthat/test_aus_pop_qtr.R
+++ tests/testthat/test_aus_pop_qtr.R
@@ -4,11 +4,15 @@ test_that("Returns correct values", {
   expect_equal(round(aus_pop_qtr("2000-Q1", allow.projections = FALSE, fertility = "high", mortality = "high.LifeExpectancy"), -4), 
                18990000)
   
-  expect_gte(aus_pop_qtr("2030-Q1", allow.projections = TRUE, fertility = "high"), 
-             aus_pop_qtr("2030-Q1", allow.projections = TRUE, fertility = "medium"))
+  x <- aus_pop_qtr("2030-Q1", allow.projections = TRUE, fertility = "high")
+  y <- aus_pop_qtr("2030-Q1", allow.projections = TRUE, fertility = "medium")
+  z <- aus_pop_qtr("2030-Q1", allow.projections = TRUE, fertility = "low")
   
-  expect_lte(aus_pop_qtr("2030-Q1", allow.projections = TRUE, fertility = "low"), 
-             aus_pop_qtr("2030-Q1", allow.projections = TRUE, fertility = "medium"))
+  expect_gte(x, 
+             y)
+  
+  expect_lte(z, 
+             y)
   
   expect_warning(aus_pop_qtr("2030-Q1", allow.projections = FALSE, fertility = "high"))
   
