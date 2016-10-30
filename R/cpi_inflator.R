97b1e967e2e1a47734934f21b7a5054d53bbace2
HughParsonage <hugh.parsonage@gmail.com>
2016-10-30 13:14:55 +1100

    Make URLs vertically aligned

--- R/cpi_inflator.R
+++ R/cpi_inflator.R
@@ -28,9 +28,14 @@ cpi_inflator <- function(from_nominal_price = 1, from_fy, to_fy = "2014-15",
   
   if (useABSConnection) {
     switch(adjustment, 
-           "none" = url <- "http://stat.data.abs.gov.au/restsdmx/sdmx.ashx/GetData/CPI/1.50.10001.10+20.Q/ABS?startTime=1948", 
-           "seasonal" = url <- "http://stat.data.abs.gov.au/restsdmx/sdmx.ashx/GetData/CPI/1.50.999901.10+20.Q/ABS?startTime=1948", 
-           "trimmed.mean" = url <- "http://stat.data.abs.gov.au/restsdmx/sdmx.ashx/GetData/CPI/1.50.999902.10+20.Q/ABS?startTime=1948")
+           "none" = url <-     
+             "http://stat.data.abs.gov.au/restsdmx/sdmx.ashx/GetData/CPI/1.50.10001.10+20.Q/ABS?startTime=1948", 
+           
+           "seasonal" = url <- 
+             "http://stat.data.abs.gov.au/restsdmx/sdmx.ashx/GetData/CPI/1.50.999901.10+20.Q/ABS?startTime=1948",
+           
+           "trimmed.mean" = url <- 
+             "http://stat.data.abs.gov.au/restsdmx/sdmx.ashx/GetData/CPI/1.50.999902.10+20.Q/ABS?startTime=1948")
     
     cpi <- rsdmx::readSDMX(url)
     message("Using ABS sdmx connection")
