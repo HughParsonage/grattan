a91addbaacdc117c4c6e4f59c296d97761fd08dc
HughParsonage <hugh.parsonage@gmail.com>
2016-11-25 11:01:11 +1100

    SAPTO now uses maximum offset for earlier years

--- R/sapto.R
+++ R/sapto.R
@@ -6,22 +6,27 @@
 #' @param fill If SAPTO was not applicable, what value should be used?
 #' @param sapto.eligible Is the individual eligible for sapto?
 #' @param family_status Family status of the individual.
+#' @export
 
-sapto <- function(rebate_income, fy.year, fill = 0,  sapto.eligible, family_status = "single"){
-  upper_threshold <- NULL; taper_rate <- NULL; max_offset <- NULL
-  input <- dplyr::data_frame(fy_year = fy.year, 
-                             family_status = family_status, 
-                             sapto.eligible = sapto.eligible,
-                             rebate_income = rebate_income)
+sapto <- function(rebate_income, fy.year, fill = 0, sapto.eligible = TRUE, family_status = "single"){
+  upper_threshold <- taper_rate <- max_offset <- NULL
+  input <- data.table(fy_year = fy.year, 
+                      family_status = family_status, 
+                      sapto.eligible = sapto.eligible,
+                      rebate_income = rebate_income)
+  ordering <- NULL
+  input[, ordering := 1:.N]
+  
+  setkeyv(input, c("fy_year", "family_status"))
   
   out <- 
-    dplyr::left_join(input, 
-                     sapto_tbl, 
-                     by = c("fy_year", "family_status")) %>%
-    dplyr::mutate(sapto = pmaxC(pminV(max_offset, 
-                                      upper_threshold * taper_rate - rebate_income * taper_rate),
-                                0)) %$%
-    sapto
+    sapto_tbl[input] %>%
+    .[,sapto := pmaxC(pminV(max_offset, 
+                           upper_threshold * taper_rate - rebate_income * taper_rate),
+                     0)] %>%
+    setkey(ordering) %>%
+    unique %>%
+    .[["sapto"]]
   
   # Eligibility for SAPTO
   out[!sapto.eligible] <- 0
