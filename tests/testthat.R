7373414b5295e0280520c1456a0f9a386ead3b5b
HughParsonage <hugh.parsonage@gmail.com>
2016-12-03 19:01:44 +1100

    Vignettes, tests new taxstats

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,10 @@
 library(testthat)
 library(grattan)
-library(taxstats)
-library(dplyr)
 library(data.table)
+library(taxstats)
+  get_sample_files_all(assign.env = .GlobalEnv)
+  sample_files_all <- sample_files_all
+library(dplyr)
 library(dtplyr)
 library(magrittr)
 library(survey)
