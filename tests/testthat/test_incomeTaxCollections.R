6ec7e101c8f7905032afda4f2160a28641330ad3
HughParsonage <hugh.parsonage@gmail.com>
2016-12-11 11:25:37 +1100

    Full income tax series

--- tests/testthat/test_incomeTaxCollections.R
+++ tests/testthat/test_incomeTaxCollections.R
@@ -34,13 +34,18 @@ test_that("Projections match collections", {
     sample_file_1314 %>%
     # ABS: 166,027 million. Cat 5506
     project_to(to_fy = "2013-14", fy.year.of.sample.file = "2013-14") %$%
-    abs(sum(income_tax(Taxable_Income, "2013-14") * WEIGHT)/ (166027 * 1e6) - 1) 
+    abs(sum(income_tax(Taxable_Income, "2013-14", 
+                       age = if_else(age_range <= 1, 67, 42), 
+                       .dots.ATO = copy(.)) * WEIGHT)/ (166027 * 1e6) - 1) 
   
   collections_1415_proj.over.actual <- 
     sample_file_1314 %>%
     # Budget papers http://www.budget.gov.au/2015-16/content/bp1/html/bp1_bs4-03.htm
     project_to(to_fy = "2014-15", fy.year.of.sample.file = "2013-14") %$%
-    abs(sum(income_tax(Taxable_Income, "2014-15") * WEIGHT) / (176600 * 1e6) - 1)
+    abs(sum(income_tax(Taxable_Income, 
+                       "2014-15", 
+                       age = if_else(age_range <= 1, 67, 42), 
+                       .dots.ATO = copy(.)) * WEIGHT) / (176600 * 1e6) - 1)
   
   # http://budget.gov.au/2016-17/content/bp1/download/bp1.pdf
   # Table 7
@@ -48,7 +53,10 @@ test_that("Projections match collections", {
     sample_file_1314 %>%
     # Budget papers http://www.budget.gov.au/2015-16/content/bp1/html/bp1_bs4-03.htm
     project_to(to_fy = "2015-16", fy.year.of.sample.file = "2013-14") %$%
-    abs(sum(income_tax(Taxable_Income, "2015-16") * WEIGHT) / (188400 * 1e6) - 1)
+    abs(sum(income_tax(Taxable_Income, 
+                       "2015-16", 
+                       age = if_else(age_range <= 1, 67, 42), 
+                       .dots.ATO = copy(.)) * WEIGHT) / (188400 * 1e6) - 1)
   
   # http://budget.gov.au/2016-17/content/bp1/download/bp1.pdf
   # Table 7
@@ -56,7 +64,10 @@ test_that("Projections match collections", {
     sample_file_1314 %>%
     # Budget papers http://www.budget.gov.au/2015-16/content/bp1/html/bp1_bs4-03.htm
     project_to(to_fy = "2016-17", fy.year.of.sample.file = "2013-14") %$%
-    abs(sum(income_tax(Taxable_Income, "2016-17") * WEIGHT) / (196950 * 1e6) - 1)
+    abs(sum(income_tax(Taxable_Income, 
+                       "2016-17", 
+                       age = if_else(age_range <= 1, 67, 42), 
+                       .dots.ATO = copy(.)) * WEIGHT) / (196950 * 1e6) - 1)
   
   expect_lt(collections_1314_proj.over.actual, 0.05)
   expect_lt(collections_1415_proj.over.actual, 0.05)
