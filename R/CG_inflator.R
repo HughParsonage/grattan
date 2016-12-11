9a6f53aed7bc43f24eca0a84b21d9e3c13699bbe
HughParsonage <hugh.parsonage@gmail.com>
2016-12-11 11:37:46 +1100

    Fix `out_tbl`

--- R/CG_inflator.R
+++ R/CG_inflator.R
@@ -4,12 +4,13 @@
 #' @param x To be inflated.
 #' @param from_fy,to_fy Financial years designating the inflation period.
 #' @param forecast.series One of \code{"mean"}, \code{"lower"}, \code{"upper"}. What estimator to use in forecasts. \code{"lower"} and \code{"upper"} give the lower and upper boundaries of the 95\% prediction interval.
+#' @param cg.series
 #' @return For \code{CG_population_inflator}, the number of individuals estimated to incur capital gains in \code{fy_year}. 
 #' For \code{CG_inflator}, an estimate of the nominal value of (total) capital gains in \code{to_fy} relative to the nominal value in \code{from_fy}. 
 
 CG_population_inflator <- function(x = 1, from_fy, to_fy, forecast.series = "mean"){
   stopifnot(all(is.fy(c(from_fy, to_fy))))
-  stopifnot(forecast.series %in% c("mean", "lower", "upper"))
+  stopifnot(forecast.series %in% c("mean", "lower", "upper", "custom"))
   
   last_fy <- max(from_fy, to_fy)
   last_year <- fy2yr(last_fy)
@@ -17,9 +18,10 @@ CG_population_inflator <- function(x = 1, from_fy, to_fy, forecast.series = "mea
   input <- 
     data.table(x = x, from_fy = from_fy, to_fy = to_fy)
   
+  
   nse_forecast_series <- forecast.series
   
-  n_cg_history <- 
+  out_tbl <- 
     cg_inflators_1314[forecast.series == nse_forecast_series]
   
   input %>%
