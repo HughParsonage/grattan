deaba9f8331b8fed51522ddea01e4f9227fd6185
HughParsonage <hugh.parsonage@gmail.com>
2016-12-16 23:21:09 +1100

    Add helper to tests
    http://stackoverflow.com/questions/41177211/devtoolstest-object-not-found/41182208#41182208

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,9 @@
 library(testthat)
 library(grattan)
+
 library(data.table)
 if (requireNamespace("taxstats", quietly = TRUE)){
   library(taxstats)
-  get_sample_files_all(globalenv())
-  sample_files_all <- sample_files_all
-  
 }
 library(dplyr)
 library(dtplyr)
---
+++ tests/testthat/helper-grattan.R
@@ -0,0 +1,6 @@
+
+if (requireNamespace("taxstats", quietly = TRUE)){
+  library(data.table)
+  library(taxstats)
+  sample_files_all <- taxstats::get_sample_files_all()
+}
