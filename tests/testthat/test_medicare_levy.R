1b13a4bf3bc76163ee17a81c9e14242edbbb5602
HughParsonage <hugh.parsonage@gmail.com>
2017-02-15 23:14:54 +1100

    Issue #50: Set to Masters Guide

--- tests/testthat/test_medicare_levy.R
+++ tests/testthat/test_medicare_levy.R
@@ -37,13 +37,31 @@ test_that("medicare_levy returns known values", {
   expect_equal(medicare_levy(20e3, "2004-05", sato = TRUE, pto = FALSE), 0)
   expect_equal(medicare_levy(16e3, "2001-02"), 240)
   expect_equal(medicare_levy(16e3, "2001-02", sapto.eligible = TRUE), 0)
+})
+
+test_that("Medicare with dependants", {
+  # skip("Issue 50 unresolved")
+  expect_equal(medicare_levy(26000, fy.year = '2014-15', Spouse_income = 26000, n_dependants = 1, family_status = 'family'), 510.40)
+  expect_equal(medicare_levy(26000, fy.year = '2014-15', Spouse_income = 26000, n_dependants = 2, family_status = 'family'), 503.55)
+  
+  expect_equal(medicare_levy(26000, fy.year = '2015-16', Spouse_income = 26000, n_dependants = 1, family_status = 'family'), 466.50)
   expect_equal(medicare_levy(24000, fy.year = '2015-16', Spouse_income = 28000, n_dependants = 3, family_status = 'family'), 67.16)
+  expect_equal(medicare_levy(26000, fy.year = '2015-16', Spouse_income = 26000, n_dependants = 2, family_status = 'family'), 415.85)
   expect_equal(medicare_levy(26000, fy.year = '2015-16', Spouse_income = 26000, n_dependants = 3, family_status = 'family'), 250.55)
-  
-  
+  expect_equal(medicare_levy(26001, fy.year = '2015-16', Spouse_income = 26000, n_dependants = 3, family_status = 'family'), 250.69)
+  expect_equal(medicare_levy(26001, fy.year = '2015-16', Spouse_income = 26001, n_dependants = 3, family_status = 'family'), 250.73)
+  expect_equal(medicare_levy(26100, fy.year = '2015-16', Spouse_income = 26100, n_dependants = 3, family_status = 'family'), 268.55)
+})
+
+test_that("Agrees with Master tax guide", {
+  expect_equal(medicare_levy(44000, fy.year = "2014-15", Spouse_income = 0, n_dependants = 2, family_status = "family"), 226.30)
+  expect_lte(abs(medicare_levy(29000, fy.year = "2014-15", Spouse_income = 27000, n_dependants = 4, family_status = "family") - 403.25), 0.05)
+  expect_lte(abs(medicare_levy(27000, fy.year = "2014-15", Spouse_income = 29000, n_dependants = 4, family_status = "family") - 375.44), 0.05)
+})
+
+test_that("Medicare error handling", {
   expect_error(medicare_levy(20e3, "2004-05", sato = TRUE, pto = TRUE))
   expect_warning(medicare_levy(20e3, "2004-05", sato = FALSE, pto = TRUE), regexp = "pto")
-  
 })
 
 
