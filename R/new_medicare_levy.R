6d04161a2b08bc32af6d23d89e3e495a9ab1908e
HughParsonage <hugh.parsonage@gmail.com>
2017-02-22 17:58:36 +1100

    Update `new_medicare_levy` reflect changes

--- R/new_medicare_levy.R
+++ R/new_medicare_levy.R
@@ -29,8 +29,9 @@ new_medicare_levy <- function(parameter_table){
                                 family_status = "individual", 
                                 n_dependants = 0, 
                                 switch){
-    stopifnot(all(family_status %in% c("family", "individual")))
-    prohibit_vector_recycling(income, family_status, Spouse_income, age, n_dependants)
+    stopifnot(all(is.fy(fy.year)), all(family_status %in% c("family", "individual")))
+    
+    prohibit_vector_recycling(income, fy.year, family_status, Spouse_income, sapto.eligible, n_dependants)
     if (any(Spouse_income > 0 & family_status == "individual")){
       stop("If Spouse_income is nonzero, family_status cannot be 'individual'.")
     }
@@ -51,18 +52,21 @@ new_medicare_levy <- function(parameter_table){
             by = c("switches"),
             sort = FALSE, 
             all.x = TRUE) %>%
-      # Levy in the case of small incomes (s.7 of Act)
-      .[, medicare_levy := pminV(pmaxC(taper * (income - lower_threshold),
-                                       0), 
-                                 rate * income)] %>%
       # Person who has spouse or dependants
       ## subs.8(5) of Act
       .[, lower_family_threshold := lower_family_threshold + n_dependants * lower_up_for_each_child] %>%
-      .[, medicare_levy := pmaxC(medicare_levy - 
-                                   (family_status == "family") *
-                                   # pmaxC <= "(if any)" subs.8(2)(c) of Medicare Levy Act 1986
-                                   pmaxC(0.02 * lower_family_threshold - 0.08 * (family_income - lower_family_threshold), 0),
-                                 0)] %>%
+      .[, upper_family_threshold := upper_family_threshold + n_dependants * lower_up_for_each_child] %>%
+      .[, income_share := if_else(Spouse_income > 0, income / (income + Spouse_income), 1)] %>%
+      # Levy in the case of small incomes (s.7 of Act)
+      .[, medicare_levy := if_else(family_status == "family" & family_income <= upper_family_threshold,
+                                   # subs.8(2)(c) of Medicare Levy Act 1986
+                                   income_share * pminV(pmaxC(taper * (family_income - lower_family_threshold), 
+                                                              0), 
+                                                        rate * family_income),
+                                   pminV(pmaxC(taper * (income - lower_threshold),
+                                               0), 
+                                         rate * income))] %>%
+      
       .[["medicare_levy"]]
   }
   
