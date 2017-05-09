057afa7f85816694eb81f5f0b797cedfb6761135
HughParsonage <hugh.parsonage@gmail.com>
2017-05-09 20:33:05 +1000

    Document residential_property_prices

---
+++ R/residential_property_prices.R
@@ -0,0 +1,12 @@
+#' @title Residential property prices in Australia
+#' @name residential_property_prices
+#' @description Residential property prices indexes for the capital cities of Australia, and a weighted average for the whole country.
+#' @format A \code{data.table} of three columns and 486 observations:
+#' \describe{
+#' \item{Date}{Date of the index}
+#' \item{City}{Capital city (or \code{Australia (weighted average)})}
+#' \item{Residential_property_price_index}{An index (100 = 2011-12-01) measuring the price change in all residential dwellings.}
+#' }
+#' @source ABS Cat 6416.0. \url{http://www.abs.gov.au/ausstats/abs@.nsf/mf/6416.0}.
+
+"residential_property_prices"
