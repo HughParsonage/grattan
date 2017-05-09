382f45318a305de1dea8d9d236d0966fc18c3ca6
HughParsonage <hugh.parsonage@gmail.com>
2017-05-09 20:23:20 +1000

    Clip redundant part of data name

--- data-raw/put-data.R
+++ data-raw/put-data.R
@@ -601,7 +601,7 @@ residential_property_prices <-
   .[order(Date)] %>%
   melt.data.table(id.vars = "Date", variable.name = "City", value.name = "Residential_property_price_index")
 
-devtools::use_data(abs_residential_property_prices)
+devtools::use_data(residential_property_prices)
 
 
 devtools::use_data(tax_table2, 
