ab6a07b917dd4694727d156a4c45c67bae5906fc
HughParsonage <hugh.parsonage@gmail.com>
2017-09-23 14:05:49 +1000

    Initial commit of Rcpp::IncomeTax

---
+++ src/IncomeTax.cpp
@@ -0,0 +1,63 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+//' @title IncomeTax
+//' @name IncomeTax
+//' @param x Taxable income.
+//' @param thresholds Lower brackets of the tax tables.
+//' @param rates Marginal rates
+//' @export IncomeTax
+
+// [[Rcpp::export]]
+NumericVector IncomeTax(NumericVector x, NumericVector thresholds, NumericVector rates) {
+  int n = x.length();
+  NumericVector tax(n);
+  
+  // Environment Env = Environment::parent();
+  
+  
+  int tn = thresholds.length();
+  
+  for (int i = 0; i < n; ++i) {
+    double xi = x[i];
+    for (int t = 1; t < tn; ++t) {
+      double t0 = thresholds[t - 1];
+      double t1 = thresholds[t];
+      double r0 = rates[t - 1];
+      if (x[i] < thresholds[t]) {
+        tax[i] += r0 * (xi - t0);
+        break;
+      } else {
+        tax[i] += r0 * (t1 - t0);
+        if (t == tn) {
+          double r1 = rates[t];
+          tax[i] += r1 * (xi - t1); 
+        }
+      }
+    }
+    
+  }
+  
+  
+  return tax;
+}
+
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically 
+// run after the compilation.
+//
+
+/*** R
+timesTwo(42)
+*/
