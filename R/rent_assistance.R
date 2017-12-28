e94605319bd695cc88d4558ae2983fe8d58a8010
HughParsonage <hugh.parsonage@gmail.com>
2017-12-28 12:10:57 +1100

    Make `verify_fys` a function

--- R/rent_assistance.R
+++ R/rent_assistance.R
@@ -36,41 +36,7 @@ rent_assistance <- function(fortnightly_rent = Inf,
         "2011-12", "2012-13", "2013-14", "2014-15", "2015-16", "2016-17", 
         "2017-18", "2018-19", "2019-20", "2020-21")
     
-    if (!all(fy.year %chin% permitted_fys)) {
-      if (any(!is.fy(fy.year))) {
-        i <- which(!is.fy(fy.year))
-        i1 <- i[1]
-        if (length(i) > 1) {
-          stop("`fy.year` contained invalid FYs. ",
-               "There were ",
-               length(i), " invalid entries (", length(i) / length(fy.year), "%).",
-               "\n\n",
-               "First invalid FY:\n\t", fy.year[i1], "\n",
-               "at position ", i)
-        } else {
-          if (length(fy.year) == 1L) {
-            stop("`fy.year` set to '", fy.year, "', which is not a valid FY. ",
-                 "Select a valid fy.year between ",
-                 permitted_fys[1], " and ", last(permitted_fys), ".")
-          } else {
-            stop("`fy.year` contained invalid entry ",
-                 fy.year[i1], " at position ", i1, ".")
-          }
-        }
-      }
-      i <- which(fy.year %notin% permitted_fys)
-      i1 <- i[1]
-      
-      if (length(fy.year) == 1L) {
-        stop("`fy.year = ", fy.year, "` was not within the allowed range: ",
-             permitted_fys[1], " <= fy.year <= ", last(permitted_fys))
-      } else {
-        stop("`fy.year` were not within the allowed range: ",
-             permitted_fys[1], " <= fy.year <= ", last(permitted_fys), "\n\n",
-             "First invalid FY:\n\t", fy.year[i], "\n",
-             "at position ", i)
-      }
-    }
+    verify_fys_permitted(fy.year, permitted_fys)
     
     prohibit_vector_recycling(fy.year, n_dependants, has_partner, fortnightly_rent)
     
--- R/unemployment_benefit.R
+++ R/unemployment_benefit.R
@@ -19,6 +19,7 @@
 unemployment_benefit <- function(income = 0,
                                  assets = 0,
                                  fy.year,
+                                 
                                  has_partner = FALSE,
                                  has_dependant = FALSE,
                                  is_home_owner = FALSE) {
@@ -35,41 +36,7 @@ unemployment_benefit <- function(income = 0,
       "2015-16", "2016-17", "2017-18", 
       "2018-19", "2019-20", "2020-21")
   
-  if (!all(fy.year %chin% permitted_fys)) {
-    if (any(!is.fy(fy.year))) {
-      i <- which(!is.fy(fy.year))
-      i1 <- i[1]
-      if (length(i) > 1) {
-        stop("`fy.year` contained invalid FYs. ",
-             "There were ",
-             length(i), " invalid entries (", length(i) / length(fy.year), "%).\n\n",
-             "First invalid FY:\n\t", fy.year[i1], "\n",
-             "at position ", i, ".")
-      } else {
-        if (length(fy.year) == 1L) {
-          stop("`fy.year` set to '", fy.year, "', which is not a valid FY. ",
-               "Select a valid fy.year between ",
-               permitted_fys[1], " and ", last(permitted_fys), ".")
-        } else {
-          stop("`fy.year` contained invalid entry ",
-               fy.year[i1], " at position ", i1, ".")
-        }
-      }
-    }
-    i <- which(fy.year %notin% permitted_fys)
-    i1 <- i[1]
-    
-    if (length(fy.year) == 1L) {
-      stop("`fy.year = ", fy.year, "` was not within the allowed range: ",
-           permitted_fys[1], " <= fy.year <= ", last(permitted_fys))
-    } else {
-      stop("`fy.year` were not within the allowed range: ",
-           permitted_fys[1], " <= fy.year <= ", last(permitted_fys), "\n\n",
-           "First invalid FY:\n\t", fy.year[i], "\n",
-           "at position ", i)
-    }
-  }
-  
+  verify_fys_permitted(fy.year, permitted_fys)
   
   prohibit_vector_recycling(income,
                             assets,
@@ -88,7 +55,9 @@ unemployment_benefit <- function(income = 0,
   
   MBR <- ES <- 
     taper_1 <- taper_2 <-
-    IncomeThreshold_1 <- IncomeThreshold_2 <- NULL
+    IncomeThreshold_1 <- IncomeThreshold_2 <- 
+    asset_cutout <- NULL
+  
   
   output <-
     unemployment_income_tests[input,
