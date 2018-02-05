6e9affb017d2c1fb79d79c668fbaea8aa7e9a919
HughParsonage <hugh.parsonage@gmail.com>
2018-02-05 15:12:08 +1100

    [Travis-devel] Spurious test failure?

--- tests/testthat/test_inverse_income.R
+++ tests/testthat/test_inverse_income.R
@@ -35,8 +35,10 @@ test_that("Inverse long income matches.", {
     fy.year <- sample(yr2fy(2004:2015), size = 1)
   }
   
-  expect_true(all(abs(inverse_income(income_tax(income, fy.year), fy.year = fy.year, zero.tax.income = "zero") - income) < 1/2), 
-              info = paste0("income <- ", paste0(income, collapse = " "), "\n", "fy.year <- ", fy.year))
+  expect_true(all(abs(inverse_income(income_tax(income, fy.year),
+                                     fy.year = fy.year,
+                                     zero.tax.income = "zero") - income) < 1/2), 
+              info = paste0("income <- ", paste0(income, collapse = ", "), "\n", "fy.year <- ", fy.year))
 })
 
 test_that("Previously failed long income matches.", {
