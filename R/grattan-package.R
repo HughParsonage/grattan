785006460f615871c5ec3f0701fba0920a13e6b9
HughParsonage <hugh.parsonage@gmail.com>
2018-06-16 21:54:13 +1000

    Import NOR

--- R/grattan-package.R
+++ R/grattan-package.R
@@ -14,6 +14,7 @@
 #' @importFrom hutils if_else
 #' @importFrom hutils coalesce
 #' @importFrom hutils OR
+#' @importFrom hutils NOR
 #' @importFrom hutils NEITHER
 #' @importFrom hutils AND
 #' @importFrom hutils %ein%
