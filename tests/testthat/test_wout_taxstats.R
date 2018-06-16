89303304e80e5a2d7c8d77c46b86f3fb8b952fbd
HughParsonage <hugh.parsonage@gmail.com>
2018-06-16 21:03:12 +1000

    Try detaching taxstats

--- tests/testthat/test_wout_taxstats.R
+++ tests/testthat/test_wout_taxstats.R
@@ -5,9 +5,16 @@ test_that("mutate_super_vars", {
   skip_if_not(identical(Sys.getenv("TRAVIS"), "true"))
   skip_if_not(identical(Sys.getenv("TRAVIS_R_VERSION_STRING"), "release"))
   skip_if_not_installed("taxstats")
+  library(utils)
+  
   if (requireNamespace("taxstats", quietly = TRUE)) {
     s1314 <- as.data.table(taxstats::sample_file_1314)
     res <- apply_super_caps_and_div293(s1314)
+    if ("taxstats" %in% .packages()) {
+      detach("package:taxstats", unload = TRUE)
+    } else if (isNamespaceLoaded("taxstats")) {
+      unloadNamespace("taxstats")
+    }
     remove.packages("taxstats")
   }
   
