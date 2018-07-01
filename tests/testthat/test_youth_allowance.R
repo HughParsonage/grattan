e04d8eba8dae8bfe1418df8ae56b0c2eab555151
Matthew Katzen <matthewkatzen@gmail.com>
2018-07-02 09:09:01 +1000

    Add youth allowance tests

--- tests/testthat/test_youth_allowance.R
+++ tests/testthat/test_youth_allowance.R
@@ -1,13 +1,74 @@
 context("Youth allowance")
 
 # http://guides.dss.gov.au/guide-social-security-law/5/5/2/40
-test_that("Works with given DSS example", {
-  skip("Not yet implemented")
-  out <- youth_allowance(ordinary_income = 180 + 10,
-                         age = 21,
-                         is_single = TRUE, 
-                         living_at_home = FALSE,
-                         has_children = FALSE, 
-                         per = "fortnight")
-  expect_equal(out, 437.50)
+test_that("Allowance with no income", {
+  expect_equal(youth_allowance(ordinary_income = 0,
+                               age=16,
+                               is_single = TRUE,
+                               living_at_home = TRUE),
+               244.10)
+  expect_equal(youth_allowance(ordinary_income = 0,
+                               age=18,
+                               is_single = TRUE,
+                               living_at_home = TRUE),
+               293.60)
+  expect_equal(youth_allowance(ordinary_income = 0,
+                               age=17,
+                               is_single = TRUE,
+                               has_children = FALSE,
+                               living_at_home = FALSE),
+               445.80)
+  expect_equal(youth_allowance(ordinary_income = 0,
+                               age = 16,
+                               is_single = TRUE,
+                               has_children = TRUE),
+               584.20)
+  expect_equal(youth_allowance(ordinary_income = 0,
+                               age = 20,
+                               isjspceoalfofcoahodeoc = TRUE),
+               762.40)
+  expect_equal(youth_allowance(ordinary_income = 0,
+                               age = 22,
+                               eligible_if_over22 = TRUE,
+                               living_at_home = TRUE),
+               360.20)
+  expect_equal(youth_allowance(ordinary_income = 0,
+                               age = 22,
+                               eligible_if_over22 = TRUE,
+                               living_at_home = FALSE),
+               541.70)
+  expect_equal(youth_allowance(ordinary_income = 0,
+                               age = 22,
+                               eligible_if_over22 = TRUE,
+                               is_single = TRUE),
+               541.70)
 })
+
+test_that("Income reduction"){
+  expect_equal(income_reduction(ordinary_income = 300,
+                                max_rate_March_2018 = 244.10,
+                                max_income = 864.84,
+                                is_student = TRUE),
+               0)
+  expect_equal(income_reduction(ordinary_income = 500,
+                                max_rate_March_2018 = 244.10,
+                                max_income = 864.84,
+                                is_student = TRUE),
+               0.5 * (500 - 437))
+  expect_equal(income_reduction(ordinary_income = 700,
+                                max_rate_March_2018 = 244.10,
+                                max_income = 864.84,
+                                is_student = TRUE),
+               0.5 * (524 - 437) + 0.6 * (700-524))
+  expect_equal(income_reduction(ordinary_income = 860,
+                                max_rate_March_2018 = 244.10,
+                                max_income = 864.84,
+                                is_student = TRUE),
+               0.5 * (524 - 437) + 0.6 * (860-524))
+  expect_equal(income_reduction(ordinary_income = 864.84,
+                                max_rate_March_2018 = 244.10,
+                                max_income = 864.84,
+                                is_student = TRUE),
+               0.5 * (524 - 437) + 0.6 * (864.84 - 524))
+  expect_equal(income_reduction())
+}
