75556a051dd88670263963e75c8c0f0d2b0a3276
Matthew Katzen <matthewkatzen@gmail.com>
2018-07-02 15:35:16 +1000

    Add comment to test re threshold error

--- tests/testthat/test_youth_allowance.R
+++ tests/testthat/test_youth_allowance.R
@@ -3,17 +3,17 @@ context("Youth allowance")
 # http://guides.dss.gov.au/guide-social-security-law/5/5/2/40
 test_that("Allowance with no income", {
   expect_equal(youth_allowance(ordinary_income = 0,
-                               age=16,
+                               age = 16,
                                is_single = TRUE,
                                living_at_home = TRUE),
                244.10)
   expect_equal(youth_allowance(ordinary_income = 0,
-                               age=18,
+                               age = 18,
                                is_single = TRUE,
                                living_at_home = TRUE),
                293.60)
   expect_equal(youth_allowance(ordinary_income = 0,
-                               age=17,
+                               age = 17,
                                is_single = TRUE,
                                has_children = FALSE,
                                living_at_home = FALSE),
@@ -55,50 +55,50 @@ test_that("Allowance with no income", {
 })
 
 test_that("Income reduction", {
-  #max income data: https://www.humanservices.gov.au/individuals/enablers/personal-income-test-austudy-and-youth-allowance/30411
+  #max income formula: https://www.humanservices.gov.au/individuals/enablers/personal-income-test-austudy-and-youth-allowance/30411
   #Student
-  expect_equal(income_reduction(ordinary_income = 300,
+  expect_equal(ya_income_reduction(ordinary_income = 300,
                                 max_rate_March_2018 = 244.10,
                                 max_income = 864.84,
                                 is_student = TRUE),
                0)
-  expect_equal(income_reduction(ordinary_income = 500,
+  expect_equal(ya_income_reduction(ordinary_income = 500,
                                 max_rate_March_2018 = 244.10,
                                 max_income = 864.84,
                                 is_student = TRUE),
                0.5 * (500 - 437))
-  expect_equal(income_reduction(ordinary_income = 700,
+  expect_equal(ya_income_reduction(ordinary_income = 700,
                                 max_rate_March_2018 = 244.10,
                                 max_income = 864.84,
                                 is_student = TRUE),
-               0.5 * (524 - 437) + 0.6 * (700-524))
-  expect_equal(income_reduction(ordinary_income = 860,
+               0.5 * (524 - 437) + 0.6 * (700 - 524))
+  expect_equal(ya_income_reduction(ordinary_income = 860,
                                 max_rate_March_2018 = 244.10,
                                 max_income = 864.84,
                                 is_student = TRUE),
-               0.5 * (524 - 437) + 0.6 * (860-524))
+               0.5 * (524 - 437) + 0.6 * (860 - 524))
   
   #Jobseeker
-  expect_equal(income_reduction(ordinary_income = 300,
+  expect_equal(ya_income_reduction(ordinary_income = 300,
                                 max_rate_March_2018 = 244.10,
                                 max_income = 864.84,
                                 is_student = TRUE),
                0)
-  expect_equal(income_reduction(ordinary_income = 500,
+  expect_equal(ya_income_reduction(ordinary_income = 500,
                                 max_rate_March_2018 = 244.10,
                                 max_income = 864.84,
                                 is_student = TRUE),
                0.5 * (500 - 437))
-  expect_equal(income_reduction(ordinary_income = 700,
+  expect_equal(ya_income_reduction(ordinary_income = 700,
                                 max_rate_March_2018 = 244.10,
                                 max_income = 864.84,
                                 is_student = TRUE),
-               0.5 * (524 - 437) + 0.6 * (700-524))
-  expect_equal(income_reduction(ordinary_income = 860,
+               0.5 * (524 - 437) + 0.6 * (700 - 524))
+  expect_equal(ya_income_reduction(ordinary_income = 860,
                                 max_rate_March_2018 = 244.10,
                                 max_income = 864.84,
                                 is_student = TRUE),
-               0.5 * (524 - 437) + 0.6 * (860-524))##ERROR: REDUCES BY MORE THAN RATE)
+               0.5 * (524 - 437) + 0.6 * (860 - 524))##ERROR: REDUCES BY MORE THAN RATE)
 })
 
 #http://guides.dss.gov.au/guide-social-security-law/5/5/2
@@ -107,18 +107,20 @@ test_that("Income reduction", {
 #})
 
 test_that('Income reduction not exceed max rate', {
-  skip('threshhold on https://www.humanservices.gov.au/individuals/enablers/personal-income-test-austudy-and-youth-allowance/30411 contradicts taper rates')
-  expect_lt(income_reduction(ordinary_income = 860,
+  skip('threshold on https://www.humanservices.gov.au/individuals/enablers/personal-income-test-austudy-and-youth-allowance/30411 contradicts taper rates')
+  #When entering income values near to the maximum income threshold, in some cases payment reduction exceeds the maximum rate. 
+  #One would expect payment reduction to approach the max rate as income approaches the max income. 
+  expect_lt(ya_income_reduction(ordinary_income = 860,
                              max_rate_March_2018 = 244.10,
                              max_income = 864.84,
                              is_student = TRUE),
             244.10)
-  expect_equal(income_reduction(ordinary_income = 864.84,
+  expect_equal(ya_income_reduction(ordinary_income = 864.84,
                                 max_rate_March_2018 = 244.10,
                                 max_income = 864.84,
                                 is_student = TRUE),
                0.5 * (524 - 437) + 0.6 * (864.84 - 524))
-  expect_equal(income_reduction(ordinary_income = 864.84,
+  expect_equal(ya_income_reduction(ordinary_income = 864.84,
                                 max_rate_March_2018 = 244.10,
                                 max_income = 864.84,
                                 is_student = TRUE),
