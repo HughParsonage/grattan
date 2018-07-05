ffafa7d6fe2ee86197394caa1f774abc1b661727
HughParsonage <hugh.parsonage@gmail.com>
2018-07-05 18:38:32 +1000

    Add assets tests for age_pension

--- tests/testthat/test_age_pension.R
+++ tests/testthat/test_age_pension.R
@@ -9,5 +9,27 @@ test_that("Maximum rates", {
 })
 
 test_that("Means testing", {
-  skip("No")
+  expect_equal(age_pension(fy.year = "2015-16",
+                           has_partner = FALSE,
+                           # http://guides.dss.gov.au/guide-social-security-law/4/10/3
+                           annual_income = c(0, 4000, 4264, 5264, 10374)), 
+               c(20745.40, 
+                 20745.40, 
+                 20745.40,
+                 20746.40 - 1000 * 0.5,
+                 0))
+  # http://guides.dss.gov.au/guide-social-security-law/4/2/3
+  # Determine the reduction for assets
+  # 
+  # Divide the assets excess by 250.
+  # Round the result DOWN to a whole number.
+  # Multiply the whole number by 19.5.
+  # RESULT: the REDUCTION FOR ASSETS.
+  expect_equal(age_pension(fy.year = "2015-16", 
+                           has_partner = FALSE, 
+                           home_owner = FALSE,
+                           # Assets limit 209,000: 
+                           # http://guides.dss.gov.au/guide-social-security-law/4/10/3
+                           assets = c(0, 209e3, 210e3)),
+               c(20745.40, 20745.40, 20745.40, 20745.40 - floor(1000/250) * 19.5))
 })
