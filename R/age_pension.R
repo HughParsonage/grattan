0218883ebbedd8adb2a8961482dfac0fcb55d2f9
HughParsonage <hugh.parsonage@gmail.com>
2018-07-06 16:14:21 +1000

    Initial commit of age_pension

---
+++ R/age_pension.R
@@ -0,0 +1,112 @@
+#' Age pension
+#' 
+#' @param ordinary_income,annual_income Income for means-testing purposes. Provide one but not both.
+#' @param has_partner (logical, default: \code{FALSE}) Does the individual have a partner?
+#' @param n_dependants How many dependants does sthe individual have? Default is zero.
+#' @param partner_fortnightly_income,partner_annual_income The partner's income. The sum of this value and the indiviudal's income gives the income test.
+#' @param Date,fy.year The financial year. Currently only 2015-16 is supported (the most recent survey of income and housing results).
+#' @param assets_value Total value of household assets.
+#' @param is_home_owner (logical, default: \code{FALSE}) Does the individual own their own home? 
+#' @param illness_separated_couple Is the couple separated by illness? (Affects the assets test.)
+#' 
+#' 
+#' @export age_pension
+#' 
+
+
+age_pension <- function(ordinary_income = 0, 
+                        annual_income = ordinary_income * 26, 
+                        has_partner = FALSE,
+                        n_dependants = 0L,
+                        partner_fortnightly_income = 0,
+                        partner_annual_income = partner_fortnightly_income * 26,
+                        Date = NULL,
+                        fy.year = NULL,
+                        assets_value = 0,
+                        is_home_owner = FALSE,
+                        illness_separated_couple = FALSE) {
+  if (is.null(Date)) {
+    if (is.null(fy.year)) {
+      stop("`Date` and `fy.year` were both NULL, yet one is required.")
+    } else {
+      if (!identical(fy.year, "2015-16")) {
+        .NotYetImplemented()
+      }
+      Date <- as.Date("2016-07-01")
+    }
+  }
+  
+  max.length <- 
+    prohibit_vector_recycling.MAXLENGTH(annual_income, 
+                                        has_partner, 
+                                        n_dependants,
+                                        partner_annual_income, 
+                                        Date, 
+                                        assets_value, 
+                                        is_home_owner)
+  
+  input <- 
+    data.table(Income = annual_income, 
+               HasPartner = has_partner, 
+               n_dependants = n_dependants,
+               PartnerIncome = partner_annual_income, 
+               Date = as.Date(Date), 
+               Assets = assets_value, 
+               HomeOwner = is_home_owner,
+               IllnessSeparated = illness_separated_couple)
+  input[, ordering := .I]
+  setkeyv(input, c("HasPartner", "Date"))
+  
+  
+  max_rates <-
+    melt.data.table(Age_pension_base_rates_by_year,
+                    id.vars = "Date", 
+                    value.name = "max_rate", 
+                    variable.factor = FALSE) %>%
+    .[, HasPartner := variable == "Married rate"] %T>%
+    # assertion that the names are correct
+    .[, stopifnot(any(HasPartner) && !all(HasPartner))] %>%
+    .[, Date := as.Date(Date)] %>%
+    setkeyv(c("HasPartner", "Date"))
+  
+  assets_test <- 
+    copy(Age_pension_assets_test_by_year) %>%
+    .[, .(HasPartner = endsWith(type, "Couple"),
+          IllnessSeparated = endsWith(type, "Illness Separated Couple"),
+          HomeOwner = startsWith(type, "Homeowner"),
+          Date = as.Date(Date),
+          assets_test)] %>%
+    setkeyv(c("HasPartner", "IllnessSeparated", "HomeOwner", "Date"))
+  
+  stopifnot(hutils::has_unique_key(assets_test))
+  
+  income_test <- 
+    Age_pension_permissible_income_by_Date %>%
+    .[, .(HasPartner = type == "Couple",
+          Date = as.Date(Date),
+          permissible_income = permissible_income)] %>%
+    setkeyv(c("HasPartner", "Date"))
+  
+  A <- max_rates[input, on = c("HasPartner", "Date"), roll = Inf, nomatch=0L]
+  B <- income_test[A, on = c("HasPartner", "Date"), roll = Inf, nomatch=0L]
+  
+  setkeyv(B, c("HasPartner", "IllnessSeparated", "HomeOwner", "Date"))
+  C <- assets_test[B, on = c("HasPartner", "IllnessSeparated", "HomeOwner", "Date"), roll = Inf, nomatch=0L] 
+
+  # print(A); print(B); print(C)
+  
+  C %>%
+    .[, age_pension_income := pminV(pmaxC(max_rate - 0.5 * (Income - permissible_income),
+                                          0),
+                                    max_rate)] %>%
+    .[, assets_excess := pmaxC(Assets - assets_test, 0)] %>%
+    .[, age_pension_assets := pminV(pmaxC(max_rate - 19.5 * floor(assets_excess / 250), 0), max_rate)] %>%
+    .[, pminV(age_pension_income, 
+              age_pension_assets)]
+  
+  
+}
+
+
+
+
--- tests/testthat/test_age_pension.R
+++ tests/testthat/test_age_pension.R
@@ -2,21 +2,21 @@ context("Age pension")
 
 test_that("Maximum rates", {
   # http://guides.dss.gov.au/guide-social-security-law/5/2/2/10
-  expect_equal(age_pension(fy.year = "2015-16", has_partner = FALSE), 20745.40)
+  expect_equal(age_pension(fy.year = "2015-16", has_partner = FALSE), 20664.80)
   expect_equal(age_pension(fy.year = "2015-16", has_partner = TRUE), 15576.60)
-  expect_equal(age_pension(Date = "2016-03-20", has_partner = FALSE), 20745.40)
-  expect_equal(age_pension(Date = "2016-03-20", has_partner = TRUE), 15576.60)
+  expect_equal(age_pension(Date = "2016-06-30", has_partner = FALSE), 20664.80)
+  expect_equal(age_pension(Date = "2016-06-30", has_partner = TRUE), 15576.60)
 })
 
 test_that("Income means testing single", {
   expect_equal(age_pension(fy.year = "2015-16",
                            has_partner = FALSE,
                            # http://guides.dss.gov.au/guide-social-security-law/4/10/3
-                           annual_income = c(0, 4000, 4264, 5264, 10374)), 
-               c(20745.40, 
-                 20745.40, 
-                 20745.40,
-                 20746.40 - 1000 * 0.5,
+                           annual_income = c(0, 4000, 4264, 5264, 50e3)), 
+               c(20664.80, 
+                 20664.80, 
+                 20664.80,
+                 20664.80 - 1000 * 0.5,
                  0))
 })
   
@@ -24,7 +24,7 @@ test_that("Income means testing couple", {
   expect_equal(age_pension(fy.year = "2015-16",
                            has_partner = TRUE,
                            # http://guides.dss.gov.au/guide-social-security-law/4/10/3
-                           annual_income = c(0, 3000, 3796, 4796, 7789)), 
+                           annual_income = c(0, 3000, 3796, 4796, 40e3)), 
                c(15576.60, 
                  15576.60, 
                  15576.60,
@@ -42,11 +42,11 @@ test_that("Assets means testing single homeowner", {
   # RESULT: the REDUCTION FOR ASSETS.
   expect_equal(age_pension(fy.year = "2015-16", 
                            has_partner = FALSE, 
-                           home_owner = TRUE,
+                           is_home_owner = TRUE,
                            # Assets limit 360500
                            # http://guides.dss.gov.au/guide-social-security-law/4/10/3
                            assets = c(0, 360.5e3, 365.5e3)),
-               c(20745.40, 20745.40, 20745.40 - floor(1000/250) * 19.5))
+               c(20664.80, 20664.80, 20664.80 - floor(1000/250) * 19.5))
 })
 
 test_that("Assets means testing single non-homeowner", {
@@ -59,11 +59,11 @@ test_that("Assets means testing single non-homeowner", {
   # RESULT: the REDUCTION FOR ASSETS.
   expect_equal(age_pension(fy.year = "2015-16", 
                            has_partner = FALSE, 
-                           home_owner = FALSE,
+                           is_home_owner = FALSE,
                            # Assets limit 209,000: 
                            # http://guides.dss.gov.au/guide-social-security-law/4/10/3
                            assets = c(0, 209e3, 210e3)),
-               c(20745.40, 20745.40, 20745.40 - floor(1000/250) * 19.5))
+               c(20664.80, 20664.80, 20664.80 - floor(1000/250) * 19.5))
 })
 
 test_that("Assets means testing couple non-homeowner", {
@@ -76,7 +76,7 @@ test_that("Assets means testing couple non-homeowner", {
   # RESULT: the REDUCTION FOR ASSETS.
   expect_equal(age_pension(fy.year = "2015-16", 
                            has_partner = TRUE, 
-                           home_owner = FALSE,
+                           is_home_owner = FALSE,
                            # Assets limit 448,000:
                            # http://guides.dss.gov.au/guide-social-security-law/4/10/3
                            assets = c(0, 448e3, 450e3)),
@@ -93,7 +93,7 @@ test_that("Assets means testing couple homeowner", {
   # RESULT: the REDUCTION FOR ASSETS.
   expect_equal(age_pension(fy.year = "2015-16", 
                            has_partner = TRUE, 
-                           home_owner = TRUE,
+                           is_home_owner = TRUE,
                            # Assets limit 296,500:
                            # http://guides.dss.gov.au/guide-social-security-law/4/10/3
                            assets = c(0, 296500, 298500)),
