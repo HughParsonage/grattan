8680fa01d4b4364f3c5da275f951726383ef915b
HughParsonage <hugh.parsonage@gmail.com>
2018-07-08 09:39:04 +1000

    Add coverage

---
+++ tests/testthat/test_verify_fys.R
@@ -0,0 +1,12 @@
+context("verify_fys_permitted")
+
+test_that("Error handling", {
+  expect_error(verify_fys_permitted(c("2015-16", "2015-17"), c("2015-16", "2016-17")), 
+               regexp = "`fy.year` contained invalid entry 2015-17 at position 2.",
+               fixed = TRUE)
+  expect_error(verify_fys_permitted(c("2015-16", "2016-17", "2017-18"),
+                                    c("2015-16", "2016-17")), 
+               regexp = "`fy.year` were not within the allowed range: 2015-16 <= fy.year <= 2016-17",
+               fixed = TRUE)
+})
+
