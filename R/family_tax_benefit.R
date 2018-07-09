eea3c25b8299478a0b7e49a459bd4704121cce36
Matthew Katzen <matthewkatzen@gmail.com>
2018-07-09 15:54:12 +1000

    Edit to take table input

--- R/family_tax_benefit.R
+++ R/family_tax_benefit.R
@@ -5,29 +5,45 @@
 #' @export
 #' 
 
-family_tax_benefit <- function(child_age = 1,
-                               child_in_secondary_school = FALSE,
-                               child_in_care = FALSE){
+family_tax_benefit <- function(data, 
+                               income_test_1_bound = 51027,
+                               income_test_2_bound = 94316,
+                               per = 'fortnight'){
+  #https://www.humanservices.gov.au/sites/default/files/co029-1603en.pdf
+  #data has columns `HH_id`, `id`, `age`, `income`, `in_secondary_school`, `in_care`
   
-  input <- data.table(do.call(cbind.data.frame, mget(ls())))
+  eligible <- data[ ,(age <= 15) | ((16 <= age) & (age <= 19) & in_secondary_school)]
   
-  max_rate_March_2016 <- 
-    input[ ,if_else(child_age < 13,
+  data[ ,max_rate_March_2016 := if_else(age < 13,
                    179.76,
-                   if_else(child_age <= 15,
+                   if_else(age <= 15,
                            233.94,
-                           if_else(child_age <= 19 & child_in_secondary_school,
+                           if_else(age <= 19 & in_secondary_school,
                                    233.94,
-                                   if_else(child_in_care,
+                                   if_else(in_care,
                                            57.68,
                                            0))))]
-    
+  
   base_rate_March_2016 <-
     57.68
+  
+  family_data <- temp %>% group_by(HH_id) %>% summarise(fam_income = sum(income), max_family_rate = sum(max_rate_March_2016))
+    
+    
+    
+  #income reduction tests
+  family_data[ ,income_test_1 := if_else(fam_income < income_test_1_bound,
+                    0,
+                    0.2 * (fam_income - income_test_1_bound))]
+  family_data[ ,income_test_2 := if_else(fam_income < income_test_2_bound,
+                    0,
+                    0.3 * (fam_income - income_test_2_bound))]
+  
+  rate <- family_data[ ,pmaxC(max_family_rate - income_test_1, base_rate_March_2016 - income_test_2)]
   
   #SUPPLEMENT?
   
   
   #OUTPUT
-  max_rate_March_2016
+  rate
 }
--- tests/testthat/test_family_tax_benefit.R
+++ tests/testthat/test_family_tax_benefit.R
@@ -1,12 +1,12 @@
 context("Family tax Benefit")
 
 test_that("Benefit with no income", {
-  expect_equal(family_tax_benefit(child_age = 0),
+  expect_equal(family_tax_benefit(data.table(HH_id = c(1, 1),pers_id = 1:2, age = c(40, 0), income = c(0,0))),
                179.76)
-  expect_equal(family_tax_benefit(child_age = 15),
+  expect_equal(family_tax_benefit(data.table(HH_id = c(1, 1),pers_id = 1:2, age = c(40, 15), income = c(0,0))),
                233.94)
-  expect_equal(family_tax_benefit(child_age = 18, child_in_secondary_school = TRUE),
+  expect_equal(family_tax_benefit(data.table(HH_id = c(1, 1),pers_id = 1:2, age = c(40, 18), income = c(0,0), in_secondary_school = c(FALSE,TRUE))),
                233.94)
-  expect_equal(family_tax_benefit(child_age = 19, child_in_care = TRUE),
+  expect_equal(family_tax_benefit(data.table(HH_id = c(1, 1),pers_id = 1:2, age = c(40, 4), income = c(0,0), in_care = c(FALSE,TRUE))),
                57.68)
 })
