d23e12d608ceb25d2f9a440471581695012df9a2
Matthew Katzen <matthewkatzen@gmail.com>
2018-07-10 16:13:33 +1000

    Add ftb tests

--- tests/testthat/test_family_tax_benefit.R
+++ tests/testthat/test_family_tax_benefit.R
@@ -1,12 +1,36 @@
 context("Family tax Benefit")
 
-test_that("Benefit with no income", {
-  expect_equal(family_tax_benefit(data.table(HH_id = c(1, 1),pers_id = 1:2, age = c(40, 0), income = c(0,0))),
-               179.76)
-  expect_equal(family_tax_benefit(data.table(HH_id = c(1, 1),pers_id = 1:2, age = c(40, 15), income = c(0,0))),
-               233.94)
-  expect_equal(family_tax_benefit(data.table(HH_id = c(1, 1),pers_id = 1:2, age = c(40, 18), income = c(0,0), in_secondary_school = c(FALSE,TRUE))),
-               233.94)
-  expect_equal(family_tax_benefit(data.table(HH_id = c(1, 1),pers_id = 1:2, age = c(40, 4), income = c(0,0), in_care = c(FALSE,TRUE))),
-               57.68)
+test_that("Benefit with no income, single family", {
+  
 })
+
+test_that("Benefit with 2 parent equal income", {
+  #no income
+  temp <- data.table(HH_id = c(1,1,1), pers_id = 1:3, age = c(35,30,5), income = c(0,0,0),
+                      in_secondary_school = F, in_care = F, single_parent = F)
+  expect_equal(family_tax_benefit(temp), 
+              data.table(HH_id = 1, ftbA_incl_supplement = 5412.95, ftbB_incl_supplement = 3139))
+  #test1
+  temp <- data.table(HH_id = c(1,1,1), pers_id = 1:3, age = c(35,30,5), income = c(30000,30000,0),
+                     in_secondary_school = F, in_care = F, single_parent = F)
+  expect_equal(family_tax_benefit(temp), 
+               data.table(HH_id = 1, ftbA_incl_supplement = 3618.35, ftbB_incl_supplement = 0))
+  
+  #base
+  temp <- data.table(HH_id = c(1,1,1), pers_id = 1:3, age = c(35,30,5), income = c(45000,45000,0),
+                      in_secondary_school = F, in_care = F, single_parent = F)
+  expect_equal(family_tax_benefit(temp), 
+               data.table(HH_id = 1, ftbA_incl_supplement = 2230.15, ftbB_incl_supplement = 0))
+  #test2
+  temp <- data.table(HH_id = c(1,1,1), pers_id = 1:3, age = c(35,30,5), income = c(50000,50000,0),
+                      in_secondary_school = F, in_care = F, single_parent = F)
+  expect_equal(family_tax_benefit(temp), 
+               data.table(HH_id = 1, ftbA_incl_supplement = 524.95, ftbB_incl_supplement = 0))
+  #max
+  temp <- data.table(HH_id = c(1,1,1), pers_id = 1:3, age = c(35,30,5), income = c(51000,51000,0),
+                     in_secondary_school = F, in_care = F, single_parent = F)
+  expect_equal(family_tax_benefit(temp), 
+               data.table(HH_id = 1, ftbA_incl_supplement = 0, ftbB_incl_supplement = 0))
+})
+
+
