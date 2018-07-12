c9ff0393beced7657da6422ec641513be560cf8e
Matthew Katzen <matthewkatzen@gmail.com>
2018-07-13 09:44:37 +1000

    Initial tests

--- R/pension_supplement.R
+++ R/pension_supplement.R
@@ -1,3 +1,10 @@
 #' Pension Supplement
 #' 
-#' 
+#' Supplementary payment for those receiving Age Pension, Carer Payment, Wife Pension, Widow B Pension, Bereavement Allowance, Disability Support Pension (except if under 21 and no kids)
+#' Can also receive if over age pension age and  receiving ABSTUDY, Austudy, Parenting Payment, Partner Allowance, Special Benefit, or, Widow Allowance
+
+pension_supplement <- function(has_partner = FALSE,
+                               n_dependants = 0){
+  max_rate_March_2016 <-
+    if_else(has_partner)
+}
--- tests/testthat/test_pension_supplement.R
+++ tests/testthat/test_pension_supplement.R
@@ -1,7 +1,10 @@
 context("Pension Supplement")
 
 test_that("", {
-  
+  test_that(pension_supplement(has_partner = FALSE),
+            65)
+  test_that(pension_supplement(has_partner = TRUE),
+            95)
   
   
   })
