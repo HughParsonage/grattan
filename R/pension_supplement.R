6425a9908fdaa74704cea1a1f37eacee938d42cc
Matthew Katzen <matthewkatzen@gmail.com>
2018-07-13 13:52:19 +1000

    Add warnings and errors

--- R/pension_supplement.R
+++ R/pension_supplement.R
@@ -1,37 +1,61 @@
 #' Pension Supplement
 #' 
-#' Gets added to max rate of payment before income reduction tests are applied
-#' Supplementary payment for those receiving Age Pension, Carer Payment, Wife Pension, Widow B Pension, Bereavement Allowance, Disability Support Pension (except if under 21 and no kids)
-#' Can also receive if over age pension age and  receiving ABSTUDY, Austudy, Parenting Payment, Partner Allowance, Special Benefit, or, Widow Allowance
-#' Can still receive the basic amount if single, under age pension age, and receive the Parenting Payment
+#' The Pension Supplement gets added to the max rate of payment before income reduction tests are applied.
+#' Can be claimed by those receiving Age Pension, Carer Payment, Wife Pension, Widow B Pension, Bereavement Allowance, or Disability Support Pension (except if under 21 and have no children).
+#' Can also receive if over age pension age and are receiving ABSTUDY, Austudy, Parenting Payment, Partner Allowance, Special Benefit, or Widow Allowance.
+#' Can still receive the basic amount if single, under age pension age, and receive the Parenting Payment.
+#'
+#' @param has_partner Does the individual have a partner?
+#' @param age The individual's age. Default is 18 years.
+#' @param n_dependants How many dependant children does the individual have?
+#' @param parenting_payment Is the individual receiving parenting payment?
+#' @param Date Date. Default is "2016/03/01" if fy.year is not present.
+#' @param fy.year Financial year. Default is "2015-16" if Date is not present.
+#' @param age_pension_age_requirement Does the payment for which the pension supplement is being added to require the individual to be over age pension age to receive? i.e. one of ABSTUDY, Austudy, Parenting Payment, Partner Allowance, Special Benefit, Widow Allowance. 
+#' @param disability_support_pension Is the individual receiving the disability support pension?
+#' @param per How often the payment will be made. Default is fortnightly. 
+#' @param overseas_absence Will the individual be living outside of Australia for more than 6 weeks of the year?
 #' @export
 
 #note: adjusted using 364/14 = 26
 
 pension_supplement <- function(has_partner = FALSE,
                                age = 70,
+                               n_dependents = 0,
                                parenting_payment = FALSE,
                                Date = NULL,
                                fy.year = NULL,
                                age_pension_age_requirement = FALSE,
+                               disability_support_pension = FALSE,
                                per = 'fortnight',
                                overseas_absence = FALSE){
   
   if (is.null(Date)) {
     if (is.null(fy.year)) {
-      Date <- "2016/03/01"
+      Date <- as.Date("2016/03/01")
+      fy.year <- "2015-16"
       warning('`Date` and `fy.year` not set, so using `Date` = "2016/03/01" and `fy.year` = "2015-16"')
     } else {
       Date <- fy2date(fy.year)
+      warning('`Date` not set. Using date as defined in fy2date()')
     }
   } else {
-    if (!is.null(fy.year)) {
+    if (is.null(fy.year)) {
+      fy.year <- date2fy(Date)
+    } else {
       warning("`fy.year` and `Date` both used. Ignoring `fy.year`.")
     }
   }
   
-  age_pension_age <-#increase:https://www.humanservices.gov.au/individuals/services/centrelink/age-pension/eligibility-payment-rates/age-rules
-                      #women increase before 2014 (not implemented): https://www.humanservices.gov.au/sites/default/files/documents/co029-0907.pdf
+  if(!(per %in% c('fortnight', 'annual'))){
+    stop("per can only take values `fortnight` or `annual`")
+  }
+  
+  input <- data.table(do.call(cbind.data.frame, mget(ls()))) #recycles vectors
+  
+  
+  input[ ,age_pension_age :=  #increase:https://www.humanservices.gov.au/individuals/services/centrelink/age-pension/eligibility-payment-rates/age-rules
+                              #women increase before 2014 (not implemented): https://www.humanservices.gov.au/sites/default/files/documents/co029-0907.pdf
     if_else(Date < "2017-07-01",
             65,
             if_else(Date < "2019-01-01",
@@ -40,30 +64,36 @@ pension_supplement <- function(has_partner = FALSE,
                             66,
                             if_else(Date < "2023-07-01",
                                     66.5,
-                                    67))))
-  
-  eligible <- if_else(age_pension_age_requirement,
-                      age > age_pension_age,
-                      TRUE)
-  
-  max_rate_March_2016 <-
+                                    67))))]
+
+  input[, eligible := if_else(age_pension_age_requirement,
+                              age >= age_pension_age,
+                              if_else(disability_support_pension,
+                                      !(age < 21 & n_dependents == 0),#ineligble if under 21 and have no kids
+                                      TRUE))]
+
+  input[ ,max_rate_March_2016 :=
     if_else(has_partner,
             49,
-            65)
+            65)]
   #not exactly sure how min rate works. Think it is the minimum payment one can receive before the individual becomes ineligible due to income reduction tests and therefore isn't used explicitly
   # min_rate_March_2016 <-
   #   if_else(has_partner,
   #           52.60,
   #           34.90)
-  basic_rate_March_2016 <-
+  input[, basic_rate_March_2016 :=
     if_else(has_partner,
             18.7,
-            22.70)
-  
-  output <- if_else(eligible,
-                    if_else((!has_partner & parenting_payment) | overseas_absence,
-                            basic_rate_March_2016,
-                            max_rate_March_2016),
-                    0)
+            22.70)]
+
+  output <- input[, if_else(eligible,
+                            if_else((!has_partner & parenting_payment) | overseas_absence,
+                                    if_else(per == 'fortnight',
+                                            basic_rate_March_2016,
+                                            basic_rate_March_2016 * 26),
+                                    if_else(per == 'fortnight',
+                                            max_rate_March_2016,
+                                            max_rate_March_2016 * 26)),
+                            0)]
   output
 }
--- tests/testthat/test_pension_supplement.R
+++ tests/testthat/test_pension_supplement.R
@@ -1,22 +1,44 @@
 context("Pension Supplement")
 
+test_that("Warnings and Errors", {
+  expect_warning(pension_supplement(),
+                 reg_exp = '`Date` and `fy.year` not set, so using `Date` = "2016/03/01" and `fy.year` = "2015-16"')
+  expect_warning(pension_supplement(fy.year = "2015-16"),
+                 reg_exp = '`Date` not set. Using date as defined in fy2date()')
+  expect_warning(pension_supplement(Date = as.Date("2016-07-01"), fy.year = "2016-17"),
+                 reg_exp = "`fy.year` and `Date` both used. Ignoring `fy.year`.")
+  expect_error(suppressWarnings(pension_supplement(per = 'month')),
+               reg_exp = "per can only take values `fortnight` or `annual`")
+})
+
 test_that("Rates", {
   #Max rates
-  test_that(suppressWarnings(pension_supplement(has_partner = FALSE)),
+  expect_equal(suppressWarnings(pension_supplement(has_partner = FALSE)),
             65)
-  test_that(suppressWarnings(pension_supplement(has_partner = TRUE)),
+  expect_equal(suppressWarnings(pension_supplement(has_partner = TRUE)),
             49)
+  expect_equal(suppressWarnings(pension_supplement(age_pension_age_requirement = TRUE, age = 65)),
+            65)
   #basic rates
-  test_that(suppressWarnings(pension_supplement(has_partner = FALSE, parenting_payment = TRUE)),
+  expect_equal(suppressWarnings(pension_supplement(has_partner = FALSE, parenting_payment = TRUE)),
             22.70)
-  test_that(suppressWarnings(pension_supplement(has_partner = FALSE, overseas_absence = TRUE)),
+  expect_equal(suppressWarnings(pension_supplement(has_partner = FALSE, overseas_absence = TRUE)),
             22.70)
-  test_that(suppressWarnings(pension_supplement(has_partner = TRUE, overseas_absence = TRUE)),
+  expect_equal(suppressWarnings(pension_supplement(has_partner = TRUE, overseas_absence = TRUE)),
             18.7)
-  })
+})
 
 test_that("Ineligibles", {
-  test_that(suppressWarnings(pension_supplement(age_pension_age_requirement = TRUE, age = 20)),
+  expect_equal(suppressWarnings(pension_supplement(age_pension_age_requirement = TRUE, age = 20)),
             0)
-  
-  })
+  expect_equal(suppressWarnings(pension_supplement(disability_support_pension = TRUE, age = 20, n_dependents = 0)),
+            0)
+  expect_equal(suppressWarnings(pension_supplement(age_pension_age_requirement = TRUE, age = 65, Date = as.Date("2017/07/01"))),
+            0)
+})
+
+test_that("Multiple People", {
+  expect_equal(suppressWarnings(pension_supplement(has_partner = c(T,F))),
+               c(49, 65))
+})
+
