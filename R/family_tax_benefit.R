b1e325e00628ee8716d535d352415671a0197175
Matthew Katzen <matthewkatzen@gmail.com>
2018-07-17 14:11:11 +1000

    add .data structure stops and tests

--- R/family_tax_benefit.R
+++ R/family_tax_benefit.R
@@ -1,6 +1,6 @@
-#' FAMILY TAX BENEFIT
+#' Family tax benefit
 #' 
-#' @param data Data table input. Each row is an individual. Column names are `HH_id`: household id, `id`: individual id, `age`: individual's age, `income`: individual's income, `in_secondary_school`: does the individual attend secondary school, `single_parent`: is the parent single, `other_allowance_benefit_or_pension`: does the individual receive a pension, benefit, or labour market program payment such as Youth Allowance, `maintenance_income`: the amount of maintenance income the individual receives for the care of a child/children from a previous relationship, `maintenance_children`: the number of children who you receive maintenance for that are in you care.
+#' @param .data .data table input. Each row is an individual. Column names are `id_hh`: household id, `id`: individual id, `age`: individual's age, `income`: individual's income, `in_secondary_school`: does the individual attend secondary school, `single_parent`: is the parent single, `other_allowance_benefit_or_pension`: does the individual receive a pension, benefit, or labour market program payment such as Youth Allowance, `maintenance_income`: the amount of maintenance income the individual receives for the care of a child/children from a previous relationship, `maintenance_children`: the number of children who you receive maintenance for that are in you care.
 #' @param income_test_ftbA_1_bound Lower bound for which reduction in ftb A max payment occurs at rate taper_ftbA_1.
 #' @param income_test_ftbA_2_bound Lower bound for which reduction in ftb A base payment occurs at rate taper_ftbA_1.
 #' @param income_test_ftbB_bound Lower bound for which reduction in ftb B payment occurs at rate taper_ftbB.
@@ -12,7 +12,7 @@
 #' @export
 #' 
 
-family_tax_benefit <- function(data, 
+family_tax_benefit <- function(.data, 
                                income_test_ftbA_1_bound = 51027,
                                income_test_ftbA_2_bound = 94316,
                                income_test_ftbB_bound = 5402,
@@ -20,19 +20,35 @@ family_tax_benefit <- function(data,
                                taper_ftbA_2 = 0.3,
                                taper_ftbB = 0.2,
                                per = 'annual'
-                               ){
+){
   #https://www.humanservices.gov.au/sites/default/files/co029-1603en.pdf
   #https://web.archive.org/web/20160420184949/http://guides.dss.gov.au/family-assistance-guide/3/1/1/20
   #historical rates: http://guides.dss.gov.au/family-assistance-guide/3/6/
   
+  #check .data in correct form
+  if(!all(c("id_hh", "id", "age", "income", "in_secondary_school", "single_parent", "other_allowance_benefit_or_pension", "maintenance_income", "maintenance_children") 
+          %in% colnames(.data))) {
+    stop("`.data` does not contain all the relevant columns.")
+  }
+  
+  if(!any(class(.data) == "data.frame")) {
+    stop("`.data` is not of class `data.frame`.")
+  }
+  
+  if(or(!all(lapply(.data, class)[c('id_hh','id', 'maintenance_income', 'maintenance_children')] 
+             %in% c('integer', 'numeric')),
+        !all(lapply(.data, class)[c('in_secondary_school', 'single_parent', 'other_allowance_benefit_or_pension')] 
+             == 'logical'))){
+    stop("`.data` columns are not of correct class. Only integer, numeric, or logical allowed.")
+  }
   
   #maintenance warning
-  if(data[ ,any((maintenance_income > 0) == (maintenance_children == 0))]){
-    stop("Incompatible combination of `maintenance_income` and `maintenance_children`")
+  if(.data[ ,any((maintenance_income > 0) == (maintenance_children == 0))]) {
+    stop("Incompatible combination of `maintenance_income` and `maintenance_children`.")
   }
   
   #ftbA: payed per child
-  data[ ,ftbA_max_rate_July_2015 := if_else(other_allowance_benefit_or_pension,
+  .data[ ,ftbA_max_rate_July_2015 := if_else(other_allowance_benefit_or_pension,
                                              0,
                                              if_else(age < 13,
                                                      179.76,
@@ -41,89 +57,89 @@ family_tax_benefit <- function(data,
                                                              if_else(age <= 19 & in_secondary_school,
                                                                      233.94,
                                                                      0))))]
-  data[ ,ftbA_base_rate_July_2015 := if_else(!other_allowance_benefit_or_pension & (age <= 15 | (age <= 19 & in_secondary_school)),
+  .data[ ,ftbA_base_rate_July_2015 := if_else(!other_allowance_benefit_or_pension & (age <= 15 | (age <= 19 & in_secondary_school)),
                                               57.68,
                                               0)]
   
-  data[ ,ftbA_supplement_July_2015 := if_else(!other_allowance_benefit_or_pension & (age <= 15 | (age <= 19 & in_secondary_school)),
+  .data[ ,ftbA_supplement_July_2015 := if_else(!other_allowance_benefit_or_pension & (age <= 15 | (age <= 19 & in_secondary_school)),
                                                726.35,
                                                0)]
-            #note 1: supplement conditional on meeting requirements e.g. immunisations, tax returns
-            #note 2: can only be paid annually
+  #note 1: supplement conditional on meeting requirements e.g. immunisations, tax returns
+  #note 2: can only be paid annually
   
   #MAINTENANCE ACTION TEST: if you care for a child from a previous relationship and do not take reasonable action to attain child support, child is ineleigible for ftb A max rate (can still receive ftbA base payment).
   
   #MAINTENANCE INCOME TEST ftbA
-  data[ ,maintenance_income_test_ftbA := if_else(maintenance_children > 0,
-                                                 if_else(maintenance_children == 1,
-                                                         pmax(0.5 * (maintenance_income - 1543.95), 0),
-                                                         pmax(0.5 * (maintenance_income - 1543.95 - (514.65 * (maintenance_children - 1))), 0)),
-                                                 0)] 
-    
-    
+  .data[ ,maintenance_income_test_ftbA := if_else(maintenance_children > 0,
+                                                  if_else(maintenance_children == 1,
+                                                          pmax(0.5 * (maintenance_income - 1543.95), 0),
+                                                          pmax(0.5 * (maintenance_income - 1543.95 - (514.65 * (maintenance_children - 1))), 0)),
+                                                  0)] 
   
-  family_data <- data %>% group_by(HH_id) %>% summarise(family_income = sum(income), 
-                                                        ftbA_max_family_rate = sum(ftbA_max_rate_July_2015), 
-                                                        ftbA_base_family_rate = sum(ftbA_base_rate_July_2015),
-                                                        ftbA_supplement_family_rate = sum(ftbA_supplement_July_2015),
-                                                        primary_income = max(income), 
-                                                        secondary_income = if_else(any(single_parent), 
-                                                                              0, 
-                                                                              sort(income, decreasing = TRUE)[2]),
-                                                        youngest_age = min(age),
-                                                        youngest_in_secondary = in_secondary_school[age==min(age)],
-                                                        youngest_allowance_benefit_or_pension = other_allowance_benefit_or_pension[age==min(age)],
-                                                        single_parent = any(single_parent),
-                                                        family_maintenance_income_test_ftbA = sum(maintenance_income_test_ftbA)) %>% data.table()
+  
+  
+  family_data <- .data %>% group_by(id_hh) %>% summarise(family_income = sum(income), 
+                                                         ftbA_max_family_rate = sum(ftbA_max_rate_July_2015), 
+                                                         ftbA_base_family_rate = sum(ftbA_base_rate_July_2015),
+                                                         ftbA_supplement_family_rate = sum(ftbA_supplement_July_2015),
+                                                         primary_income = max(income), 
+                                                         secondary_income = if_else(any(single_parent), 
+                                                                                    0, 
+                                                                                    sort(income, decreasing = TRUE)[2]),
+                                                         youngest_age = min(age),
+                                                         youngest_in_secondary = in_secondary_school[age==min(age)],
+                                                         youngest_allowance_benefit_or_pension = other_allowance_benefit_or_pension[age==min(age)],
+                                                         single_parent = any(single_parent),
+                                                         family_maintenance_income_test_ftbA = sum(maintenance_income_test_ftbA)) %>% data.table()
   
   #ftbB: payed based on youngest child
-      #cannot be paid during Paid Parental Leave period
-      #note: eligibility changed for fy 2016-17: https://www.humanservices.gov.au/sites/default/files/co029-1607en.pdf
+  #cannot be paid during Paid Parental Leave period
+  #note: eligibility changed for fy 2016-17: https://www.humanservices.gov.au/sites/default/files/co029-1607en.pdf
   ftbB_rate_July_2015 <- family_data[ ,if_else(youngest_allowance_benefit_or_pension,
-                                                0,
-                                                if_else(youngest_age < 5,
+                                               0,
+                                               if_else(youngest_age < 5,
                                                        152.88,
                                                        if_else(youngest_age <= 15 | (youngest_age <= 19 & youngest_in_secondary),
-                                                                       106.82,
-                                                                       0)))]
+                                                               106.82,
+                                                               0)))]
   
   ftbB_supplement_July_2015 <- family_data[ ,if_else(!youngest_allowance_benefit_or_pension & 
-                                                      (youngest_age <= 15 | (youngest_age <= 19 & youngest_in_secondary)),
-                                                      354.05,
-                                                      0)]
-
-
+                                                       (youngest_age <= 15 | (youngest_age <= 19 & youngest_in_secondary)),
+                                                     354.05,
+                                                     0)]
+  
+  
   #income reduction test ftbA
   income_test_ftbA_1 <- family_data[ ,if_else(family_income < income_test_ftbA_1_bound,
-                    0,
-                    taper_ftbA_1 * (family_income - income_test_ftbA_1_bound))]
-
+                                              0,
+                                              taper_ftbA_1 * (family_income - income_test_ftbA_1_bound))]
+  
   income_test_ftbA_2 <- family_data[ ,if_else(family_income < income_test_ftbA_2_bound,
-                    0,
-                    taper_ftbA_2 * (family_income - income_test_ftbA_2_bound))]
-            #note: before 2015 income_test_ftbA_2_bound increased based upon number of ftb children http://guides.dss.gov.au/family-assistance-guide/3/6/1 note 2G
-
+                                              0,
+                                              taper_ftbA_2 * (family_income - income_test_ftbA_2_bound))]
+  #note: before 2015 income_test_ftbA_2_bound increased based upon number of ftb children http://guides.dss.gov.au/family-assistance-guide/3/6/1 note 2G
+  
   #income test ftbB
   ftbB_eligible <- family_data[ ,primary_income < 100000]
-
+  
   income_test_ftbB <- family_data[ ,if_else(primary_income < 100000,
-                                             if_else(single_parent,
-                                                     0,
-                                                     if_else(secondary_income > income_test_ftbB_bound,
-                                                             taper_ftbB * (secondary_income - income_test_ftbB_bound),
-                                                             0)),
-                                             0)]
-
-    output<- data.table(HH_id = family_data$HH_id,
-                      ftbA_incl_supplement = family_data[ ,pmax(365/14 * ftbA_max_family_rate + ftbA_supplement_family_rate - income_test_ftbA_1 - family_maintenance_income_test_ftbA, 
-                                                         365/14 * ftbA_base_family_rate + ftbA_supplement_family_rate - income_test_ftbA_2,
-                                                         0)],
-
-                      ftbB_incl_supplement = family_data[ ,if_else(ftbB_eligible, 
-                                                                   pmaxC(365/14 * ftbB_rate_July_2015 + ftbB_supplement_July_2015 - income_test_ftbB,
-                                                                         0), 
-                                                                   0)])
-                      
-
+                                            if_else(single_parent,
+                                                    0,
+                                                    if_else(secondary_income > income_test_ftbB_bound,
+                                                            taper_ftbB * (secondary_income - income_test_ftbB_bound),
+                                                            0)),
+                                            0)]
+  
+  output<- data.table(id_hh = family_data$id_hh,
+                       ftbA_incl_supplement = family_data[ ,pmax(365/14 * ftbA_max_family_rate + ftbA_supplement_family_rate - income_test_ftbA_1 - family_maintenance_income_test_ftbA, 
+                                                                 365/14 * ftbA_base_family_rate + ftbA_supplement_family_rate - income_test_ftbA_2,
+                                                                 0)],
+                       
+                       ftbB_incl_supplement = family_data[ ,if_else(ftbB_eligible, 
+                                                                    pmaxC(365/14 * ftbB_rate_July_2015 + ftbB_supplement_July_2015 - income_test_ftbB,
+                                                                          0), 
+                                                                    0)])
+  
+  
   output
 }
--- tests/testthat/test_family_tax_benefit.R
+++ tests/testthat/test_family_tax_benefit.R
@@ -1,95 +1,112 @@
 context("Family tax Benefit")
 
 test_that("Fringe cases", {
+  library(data.table)
   #no income
-  temp <- data.table(HH_id = c(1, 1, 1), pers_id = 1:3, age = c(35, 30, 0), income = c(0, 0, 0),
+  temp <- data.table(id_hh = c(1, 1, 1), id = 1:3, age = c(35, 30, 0), income = c(0, 0, 0),
                      in_secondary_school = F, single_parent = F, other_allowance_benefit_or_pension = F, maintenance_income = 0, maintenance_children = 0)
   expect_equal(family_tax_benefit(temp), 
-               data.table(HH_id = 1, ftbA_incl_supplement = 5412.95, ftbB_incl_supplement = 4339.85))
+               data.table(id_hh = 1, ftbA_incl_supplement = 5412.95, ftbB_incl_supplement = 4339.85))
   #single parent
-  temp <- data.table(HH_id = c(1, 1), pers_id = 1:2, age = c(35, 10), income = c(70000, 0),
+  temp <- data.table(id_hh = c(1, 1), id = 1:2, age = c(35, 10), income = c(70000, 0),
                      in_secondary_school = F, single_parent = T, other_allowance_benefit_or_pension = F, maintenance_income = 0, maintenance_children = 0)
   expect_equal(family_tax_benefit(temp), 
-               data.table(HH_id = 1, ftbA_incl_supplement = 2230.15, ftbB_incl_supplement = 3138.99),
+               data.table(id_hh = 1, ftbA_incl_supplement = 2230.15, ftbB_incl_supplement = 3138.99),
                tol=1e-02)
   #18 yo child not in secondary school
-  temp <- data.table(HH_id = c(1, 1), pers_id = 1:2, age = c(35, 18), income = c(70000, 0),
+  temp <- data.table(id_hh = c(1, 1), id = 1:2, age = c(35, 18), income = c(70000, 0),
                      in_secondary_school = F, single_parent = c(T, F), other_allowance_benefit_or_pension = F, maintenance_income = 0, maintenance_children = 0)
   expect_equal(family_tax_benefit(temp), 
-               data.table(HH_id = 1, ftbA_incl_supplement = 0, ftbB_incl_supplement = 0))
+               data.table(id_hh = 1, ftbA_incl_supplement = 0, ftbB_incl_supplement = 0))
   #child receieving youth allowance
-  temp <- data.table(HH_id = c(1, 1), pers_id = 1:2, age = c(35, 10), income = c(70000, 0),
+  temp <- data.table(id_hh = c(1, 1), id = 1:2, age = c(35, 10), income = c(70000, 0),
                      in_secondary_school = F, single_parent = T, other_allowance_benefit_or_pension = c(F, T) , maintenance_income = 0, maintenance_children = 0)
   expect_equal(family_tax_benefit(temp), 
-               data.table(HH_id = 1, ftbA_incl_supplement = 0, ftbB_incl_supplement = 0),
+               data.table(id_hh = 1, ftbA_incl_supplement = 0, ftbB_incl_supplement = 0),
                tol=1e-02)
 })
 
 test_that("Benefit with 2 parent equal income", {
+  library(data.table)
   #no income
-  temp <- data.table(HH_id = c(1, 1, 1), pers_id = 1:3, age = c(35, 30, 5), income = c(0, 0, 0),
+  temp <- data.table(id_hh = c(1, 1, 1), id = 1:3, age = c(35, 30, 5), income = c(0, 0, 0),
                       in_secondary_school = F, single_parent = F, other_allowance_benefit_or_pension = F, maintenance_income = 0, maintenance_children = 0)
   expect_equal(family_tax_benefit(temp), 
-              data.table(HH_id = 1, ftbA_incl_supplement = 5412.95, ftbB_incl_supplement = 3139))
+              data.table(id_hh = 1, ftbA_incl_supplement = 5412.95, ftbB_incl_supplement = 3139))
   #test1
-  temp <- data.table(HH_id = c(1, 1, 1), pers_id = 1:3, age = c(35, 30, 5), income = c(30000, 30000, 0),
+  temp <- data.table(id_hh = c(1, 1, 1), id = 1:3, age = c(35, 30, 5), income = c(30000, 30000, 0),
                      in_secondary_school = F, single_parent = F, other_allowance_benefit_or_pension = F, maintenance_income = 0, maintenance_children = 0)
   expect_equal(family_tax_benefit(temp), 
-               data.table(HH_id = 1, ftbA_incl_supplement = 3618.35, ftbB_incl_supplement = 0))
+               data.table(id_hh = 1, ftbA_incl_supplement = 3618.35, ftbB_incl_supplement = 0))
   #base
-  temp <- data.table(HH_id = c(1, 1, 1), pers_id = 1:3, age = c(35, 30, 5), income = c(45000, 45000, 0),
+  temp <- data.table(id_hh = c(1, 1, 1), id = 1:3, age = c(35, 30, 5), income = c(45000, 45000, 0),
                       in_secondary_school = F, single_parent = F, other_allowance_benefit_or_pension = F, maintenance_income = 0, maintenance_children = 0)
   expect_equal(family_tax_benefit(temp), 
-               data.table(HH_id = 1, ftbA_incl_supplement = 2230.15, ftbB_incl_supplement = 0))
+               data.table(id_hh = 1, ftbA_incl_supplement = 2230.15, ftbB_incl_supplement = 0))
   #test2
-  temp <- data.table(HH_id = c(1, 1, 1), pers_id = 1:3, age = c(35, 30, 5), income = c(50000, 50000, 0),
+  temp <- data.table(id_hh = c(1, 1, 1), id = 1:3, age = c(35, 30, 5), income = c(50000, 50000, 0),
                       in_secondary_school = F, single_parent = F, other_allowance_benefit_or_pension = F, maintenance_income = 0, maintenance_children = 0)
   expect_equal(family_tax_benefit(temp), 
-               data.table(HH_id = 1, ftbA_incl_supplement = 524.95, ftbB_incl_supplement = 0))
+               data.table(id_hh = 1, ftbA_incl_supplement = 524.95, ftbB_incl_supplement = 0))
   #max
-  temp <- data.table(HH_id = c(1, 1, 1), pers_id = 1:3, age = c(35, 30, 5), income = c(51000, 51000, 0),
+  temp <- data.table(id_hh = c(1, 1, 1), id = 1:3, age = c(35, 30, 5), income = c(51000, 51000, 0),
                      in_secondary_school = F, single_parent = F, other_allowance_benefit_or_pension = F, maintenance_income = 0, maintenance_children = 0)
   expect_equal(family_tax_benefit(temp), 
-               data.table(HH_id = 1, ftbA_incl_supplement = 0, ftbB_incl_supplement = 0))
+               data.table(id_hh = 1, ftbA_incl_supplement = 0, ftbB_incl_supplement = 0))
 })
 
 test_that("Maintenance", {
+  library(data.table)
   #1 child
-  temp <- data.table(HH_id = c(1, 1, 1), pers_id = 1:3, age = c(35, 30, 5), income = c(30000, 30000, 0),
+  temp <- data.table(id_hh = c(1, 1, 1), id = 1:3, age = c(35, 30, 5), income = c(30000, 30000, 0),
                      in_secondary_school = F, single_parent = F, other_allowance_benefit_or_pension = F, maintenance_income = c(3000, 0, 0), maintenance_children = c(1, 0, 0))
   expect_equal(family_tax_benefit(temp), 
-               data.table(HH_id = 1, ftbA_incl_supplement = 2889.95, ftbB_incl_supplement = 0),
+               data.table(id_hh = 1, ftbA_incl_supplement = 2889.95, ftbB_incl_supplement = 0),
                tol = 1e-02)
   #2 children same parent
-  temp <- data.table(HH_id = c(1, 1, 1, 1), pers_id = 1:4, age = c(35, 30, 5, 0), income = c(30000, 30000, 0, 0),
+  temp <- data.table(id_hh = c(1, 1, 1, 1), id = 1:4, age = c(35, 30, 5, 0), income = c(30000, 30000, 0, 0),
                      in_secondary_school = F, single_parent = F, other_allowance_benefit_or_pension = F, maintenance_income = c(6000, 0, 0, 0), maintenance_children = c(2, 0, 0, 0))
   expect_equal(family_tax_benefit(temp), 
-               data.table(HH_id = 1, ftbA_incl_supplement =  7060.6, ftbB_incl_supplement = 0),
+               data.table(id_hh = 1, ftbA_incl_supplement =  7060.6, ftbB_incl_supplement = 0),
                tol = 1e-02)
   #3 children different parents
-  temp <- data.table(HH_id = c(1, 1, 1, 1, 1), pers_id = 1:5, age = c(35, 30, 5, 0, 15), income = c(30000, 30000, 0, 0, 0),
+  temp <- data.table(id_hh = c(1, 1, 1, 1, 1), id = 1:5, age = c(35, 30, 5, 0, 15), income = c(30000, 30000, 0, 0, 0),
                      in_secondary_school = F, single_parent = F, other_allowance_benefit_or_pension = F, maintenance_income = c(6000, 2000, 0, 0, 0), maintenance_children = c(2, 1, 0, 0, 0))
   expect_equal(family_tax_benefit(temp), 
-               data.table(HH_id = 1, ftbA_incl_supplement =  13658.08, ftbB_incl_supplement = 0),
+               data.table(id_hh = 1, ftbA_incl_supplement =  13658.08, ftbB_incl_supplement = 0),
                tol = 1e-02)
 })
 
 test_that("Multiple HH - combos of previous tests", {
-  temp <- data.table(HH_id = c(1, 1, 1, 2, 2, 3, 3, 3, 3, 3), pers_id = 1:10, age = c(35, 30, 5, 35, 10, 35, 30, 5, 0, 15), income = c(50000, 50000, 0, 70000, 0, 30000, 30000, 0, 0, 0),
+  library(data.table)
+  temp <- data.table(id_hh = c(1, 1, 1, 2, 2, 3, 3, 3, 3, 3), id = 1:10, age = c(35, 30, 5, 35, 10, 35, 30, 5, 0, 15), income = c(50000, 50000, 0, 70000, 0, 30000, 30000, 0, 0, 0),
                      in_secondary_school = F, single_parent = c(F, F, F, T, F, F, F, F, F, F), other_allowance_benefit_or_pension = F, 
                      maintenance_income = c(0, 0, 0, 0, 0, 6000, 2000, 0, 0, 0), maintenance_children = c(0, 0, 0, 0, 0, 2, 1, 0, 0, 0))
   expect_equal(family_tax_benefit(temp), 
-               data.table(HH_id = c(1,2, 3), ftbA_incl_supplement = c(524.95, 2230.15, 13658.08), ftbB_incl_supplement = c(0, 3138.99, 0)),
+               data.table(id_hh = c(1,2, 3), ftbA_incl_supplement = c(524.95, 2230.15, 13658.08), ftbB_incl_supplement = c(0, 3138.99, 0)),
                tol = 1e-02)
 })
 
 test_that("Errors", {
-  temp <- data.table(HH_id = c(1, 1, 1), pers_id = 1:3, age = c(35, 30, 0), income = c(0, 0, 0),
+  library(data.table)
+  temp <- data.table(id_hh = c(1, 1, 1), id = 1:3, age = c(35, 30, 0), income = c(0, 0, 0),
                      in_secondary_school = F, single_parent = F, other_allowance_benefit_or_pension = F, maintenance_income = c(100, 0, 0), maintenance_children = 0)
   expect_error(family_tax_benefit(temp), 
                  regexp = "Incompatible combination of `maintenance_income` and `maintenance_children`")
-  temp <- data.table(HH_id = c(1, 1, 1), pers_id = 1:3, age = c(35, 30, 0), income = c(0, 0, 0),
+  temp <- data.table(id_hh = c(1, 1, 1), id = 1:3, age = c(35, 30, 0), income = c(0, 0, 0),
                      in_secondary_school = F, single_parent = F, other_allowance_benefit_or_pension = F, maintenance_income = 0, maintenance_children = c(1,0,0))
   expect_error(family_tax_benefit(temp), 
                  regexp = "Incompatible combination of `maintenance_income` and `maintenance_children`")
+  temp <- data.table(ERROR = c(1, 1, 1), id = 1:3, age = c(35, 30, 0), income = c(0, 0, 0),
+                     in_secondary_school = F, single_parent = F, other_allowance_benefit_or_pension = F, maintenance_income = 0, maintenance_children = c(1,0,0))
+  expect_error(family_tax_benefit(temp), 
+               regexp = "`.data` does not contain all the relevant columns.")
+  temp <- data.matrix(data.frame(id_hh = c(1, 1, 1), id = 1:3, age = c(35, 30, 0), income = c(0, 0, 0),
+                     in_secondary_school = F, single_parent = F, other_allowance_benefit_or_pension = F, maintenance_income = 0, maintenance_children = c(1,0,0)))
+  expect_error(family_tax_benefit(temp), 
+               regexp = "`.data` is not of class `data.frame`.")
+  temp <- data.frame(id_hh = c('1', 1, 1), id = 1:3, age = c(35, 30, 0), income = c(0, 0, 0),
+                                 in_secondary_school = F, single_parent = F, other_allowance_benefit_or_pension = F, maintenance_income = 0, maintenance_children = c(1,0,0))
+  expect_error(family_tax_benefit(temp), 
+               regexp = "`.data` columns are not of correct class. Only integer, numeric, or logical allowed.")
 })
