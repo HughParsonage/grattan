6c1f45075f0780e7c0d14341bfca45f324e4d515
HughParsonage <hugh.parsonage@gmail.com>
2018-07-19 01:52:33 +1000

    Initial commit of standardize

---
+++ R/standardize_payment_names.R
@@ -0,0 +1,34 @@
+
+
+standardize_payment_names <- function(payment_name) {
+  .pn <-
+    payment_name %>%
+    trimws %>%
+    tolower %>%
+    gsub("[^a-z]+", " ", x = .)
+  
+  Switch(.pn, 
+         "abstudy" = "abstudy",
+         "austudy" = "austudy",
+         "dsp" = "disability pension",
+         "disability support pension" = "disability pension",
+         
+         "ftba" = "ftba",
+         "ftb a" = "ftba",
+         "family tax benefit a" = "ftba",
+         
+         "ftb" = "ftbb",
+         "ftb b" = "ftbb",
+         "family tax benefit b" = "ftbb",
+         
+         "nsa" = "newstart allowance",
+         "newstart" = "newstart allowance", 
+         
+         "rent assistance" = "rent assistance",
+         "commonwealth rent assistance" = "rent assistance",
+         "cra" = "rent assistance",
+         
+         "ya" = "youth allowance")
+}
+
+
