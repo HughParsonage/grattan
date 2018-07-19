340c163bb645989ca53ff7e7747e41769b1d7b9f
Matthew Katzen <matthewkatzen@gmail.com>
2018-07-19 13:40:31 +1000

    add .fortnight_per_year argument

--- R/validate_per.R
+++ R/validate_per.R
@@ -1,7 +1,7 @@
 #' Validate per
- 
+#' @export validate_per
 
-validate_per <- function(per, missing_per) {
+validate_per <- function(per, missing_per, .fortnights_per_yr = NULL) {
   if (missing_per) {
     per <- per[1L]
     message("`per` not set; calculating ", paste0(per, "ly"), " payment.")
@@ -20,4 +20,28 @@ validate_per <- function(per, missing_per) {
       stop("`per = '", per, "'` but must be one of 'year', 'fortnight', or 'quarter'. ")
     }
   }
+  
+  if (per == "fortnight") {
+    if (is.null(.fortnights_per_yr)) {
+      .fortnights_per_yr <- 26
+      warning("`.fortnights_per_year` not set. Using 26 as default.")
+    }
+    if (.fortnights_per_yr == 26) {
+      26
+    } else if (.fortnights_per_yr == 365/14){
+      365/14
+    } else if (!is.numeric(.fortnights_per_yr)){
+      stop("`per` was type '", typeof(per), "', but must be numeric.")
+    } else {
+      stop("`.fortnights_per_yr` must be either 26 or 365/14")
+    } 
+    
+  } else if(per == "quarter") {
+    4
+  } else if(per == "year") {
+    1
+  }
+    
 }
+  
+  
--- tests/testthat/test_validate_per.R
+++ tests/testthat/test_validate_per.R
@@ -1,13 +1,30 @@
 context('Validate per')
 
 test_that("Errors", {
-  expect_message(validate_per(per = 'year', missing_per = TRUE),
-              regexp = "`per` not set; calculating yearly payment.")
-  expect_error(validate_per(per = NULL, missing_per = FALSE),
+  expect_message(validate_per(per = 'year', missing_per = TRUE, .fortnights_per_yr = "26"),
+                 regexp = "`per` not set; calculating yearly payment.")
+  expect_error(validate_per(per = NULL, missing_per = FALSE, .fortnights_per_yr = "26"),
                regexp = "`per` was type 'NULL', but must be a string.")
-  expect_error(validate_per(per = "month", missing_per = FALSE),
+  expect_error(validate_per(per = "month", missing_per = FALSE, .fortnights_per_yr = "26"),
                regexp = "`per = 'month'` but must be one of 'year', 'fortnight', or 'quarter'.")
-  expect_warning(validate_per(per = c("fortnight", "month"), missing_per = FALSE),
-               regexp = "`per` is provided but has length > 1 so only the first element (`per = 'fortnight'`) will be used.",
-               fixed = TRUE)
+  expect_warning(validate_per(per = c("fortnight", "month"), missing_per = FALSE, .fortnights_per_yr = "26"),
+                 regexp = "`per` is provided but has length > 1 so only the first element (`per = 'fortnight'`) will be used.",
+                 fixed = TRUE)
+  expect_error(validate_per(per = "fortnight", missing_per = FALSE, .fortnights_per_yr = 360/26),
+               reg_exp = "`.fortnights_per_yr` must be either 26 or 365/14")
+  expect_error(validate_per(per = "fortnight", missing_per = FALSE, .fortnights_per_yr = "twentysix"),
+               reg_exp = "`per` was type 'character', but must be numeric.")
+  expect_warning(validate_per(per = "fortnight", missing_per = FALSE),
+                 regexp = "`.fortnights_per_year` not set. Using 26 as default.")
+})
+
+test_that("Values", {
+  expect_equal(validate_per("fortnight", missing_per = FALSE, .fortnights_per_yr = 26), 
+               26)
+  expect_equal(validate_per("fortnight", missing_per = FALSE, .fortnights_per_yr = 365/14), 
+               365/14)
+  expect_equal(validate_per("quarter", missing_per = FALSE, .fortnights_per_yr = "26"), 
+               4)
+  expect_equal(validate_per("year", missing_per = FALSE, .fortnights_per_yr = "26"), 
+               1)
 })
