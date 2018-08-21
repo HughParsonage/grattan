a313d0796ad7d2107e29e2f0c734ef6ded879a8e
Matthew Katzen <matthewkatzen@gmail.com>
2018-08-21 15:24:55 +1000

    Add attribute

--- R/verify_date.R
+++ R/verify_date.R
@@ -1,27 +1,49 @@
+#' Verifying validity of dates
+#' 
+#' @description Many functions expect Dates. 
+#' Determining that they are validly entered is often quite 
+#' computatationally costly, relative to the core calculations.
+#' These internal functions provide mechanisms to check validity
+#' quickly, while still providing clear, accurate error messages.
+#' 
+#' @param date_to_verify (character) A user-provided value, purporting to be
+#' character vector of dates.
+#' @param from,to (integer) Indicating the range of years valid for date_to_verify. Default set to -Inf and Inf respectively (i.e. there is no bound)
+#' 
+#' @return If \code{date_to_verify} contains valid dates
+#' they are returned as date objects. If they were
+#' already in that form, they obtain the following attributes:
+#' \describe{
+#' \item{\code{grattan_all_date}}{\code{TRUE} if all the dates are valid.}
+#' 
 
-verify_date <- function(date_to_verify, from , to) {
-  Date <- date_to_verify
-  if (missing(from)){
-    from <- -Inf
+verify_date <- function(date_to_verify, from = -Inf, to = Inf) {
+
+  if (isTRUE(attr(date_to_verify, "grattan_all_date"))) {
+    # If from and to are fine, we're done
+    if (missing(from) && missing(to)) {
+      return(date_to_verify)
+    }
   }
-  if (missing(to)){
-    to <- Inf
-  }
+  
   # check if able to make object a Date
-  if (any(is.na(as.Date(Date, optional = TRUE)))) {
-    i_bad_date <- which(is.na(as.Date(Date, optional = TRUE)))
+  if (any(is.na(as.Date(date_to_verify, optional = TRUE)))) {
+    i_bad_date <- which(is.na(as.Date(date_to_verify, optional = TRUE)))
     first_bad_date_i <- i_bad_date[1]
-    first_bad_date <- Date[first_bad_date_i]
+    first_bad_date <- date_to_verify[first_bad_date_i]
     stop("`Date` had value ", first_bad_date, " at position ", first_bad_date_i, ". ")
   }
   
   # check if in correct timeframe
-  if (!all(between(year(Date), from, to))) {
-    i_bad_date <- which(!between(year(Date), from, to))
+  if (!all(between(year(date_to_verify), from, to))) {
+    i_bad_date <- which(!between(year(date_to_verify), from, to))
     first_bad_date_i <- i_bad_date[1]
-    first_bad_date <- Date[first_bad_date_i]
+    first_bad_date <- date_to_verify[first_bad_date_i]
     stop("`Date` had value ", first_bad_date, " at position ", first_bad_date_i, ". ",
          "Ensure `Date` only includes dates between ", from, " and ", to)
   }
+  #give output tick of approval
+  attr(date_to_verify, "grattan_all_date") <- TRUE
   
+  return(date_to_verify)
 }
--- tests/testthat/test_verify_date.R
+++ tests/testthat/test_verify_date.R
@@ -1,11 +1,41 @@
 context("Verify Date")
 
 test_that("errors", {
-  expect_silent(verify_date("2018-10-10"))#expect no output
   expect_error(verify_date("2018-10-10", from = 2010, to = 2011),
                regexp = "`Date` had value 2018-10-10 at position 1. Ensure `Date` only includes dates between 2010 and 2011")
+  expect_error(verify_date("2018-10-10", to = 2011),
+               regexp = "`Date` had value 2018-10-10 at position 1. Ensure `Date` only includes dates between -Inf and 2011")
+  expect_error(verify_date("2018-10-10", from = 2019),
+               regexp = "`Date` had value 2018-10-10 at position 1. Ensure `Date` only includes dates between 2019 and Inf")
   expect_error(verify_date("cvgvg"),
                regexp = "`Date` had value cvgvg at position 1.")
   expect_error(verify_date(c("2018-10-10","cvgvg"), from = 2010, to = 2011),
                regexp = "`Date` had value cvgvg at position 2.")
+})
+
+test_that("values", {
+  x <- "2018-10-10"
+  x2 <- verify_date(x)
+  expect_equal(x2, x, check.attributes = FALSE)
+  expect_true(attributes(x2)$grattan_all_date)
+
+  x <- c("2018-10-10","2018-10-11")
+  x2 <- verify_date(x)
+  expect_equal(x2, x, check.attributes = FALSE)
+  expect_true(attributes(x2)$grattan_all_date)
+  
+  x <- c("2018-10-10","2018-10-11")
+  x2 <- verify_date(x, from = 2018, to = 2019)
+  expect_equal(x2, x, check.attributes = FALSE)
+  expect_true(attributes(x2)$grattan_all_date)
+  
+  x <- c("2018-10-10","2018-10-11")
+  x2 <- verify_date(x, from = 2018)
+  expect_equal(x2, x, check.attributes = FALSE)
+  expect_true(attributes(x2)$grattan_all_date)
+  
+  x <- c("2018-10-10","2018-10-11")
+  x2 <- verify_date(x, to = 2019)
+  expect_equal(x2, x, check.attributes = FALSE)
+  expect_true(attributes(x2)$grattan_all_date)
 })
