7a91dcbf49828f2ca73eb65ef0d0c7c470c19fc1
Matthew Katzen <matthewkatzen@gmail.com>
2018-08-22 16:59:49 +1000

    Add baseline_fy, baseline_date, and return.

--- R/model_rent_assistance.R
+++ R/model_rent_assistance.R
@@ -1,5 +1,13 @@
 #' Model Rent Assistance
 #' 
+#' @param sample_file A sample file having the same variables as the data.frame in the example.
+#' @param .Prop_rent_paid_by_RA The proportion of the rent above the minimum threshold paid by rent assistance. 
+#' @param Max_rate If not \code{NULL}, a numeric vector indicating for each individual the maximum rent assistance payable.
+#' @param Min_rent If not \code{NULL}, a numeric vector indicating for each individual the minimum fortnightly rent above which rent assistance is payable. \code{max_rate} and \code{min_rent}
+#' @param return. What should the function return? One of \code{tax}, \code{sample_file}, or \code{sample_file.int}. 
+#' If \code{tax}, the tax payable under the settings; if \code{sample_file}, the \code{sample_file},
+#' but with variables \code{tax} and possibly \code{new_taxable_income}; if \code{sample_file.int}, same as \code{sample_file} but \code{new_tax} is coerced to integer.
+
 #' @export 
 #' 
 #' @examples
@@ -7,9 +15,13 @@
 #' model_rent_assistance(sample, .Prop_rent_paid_by_RA = 0.75, Max_rate = 500, Min_rent = 100)
 
 model_rent_assistance <- function(sample_file,
+                                  baseline_fy = NULL,
+                                  baseline_Date = NULL,
                                   .Prop_rent_paid_by_RA,
                                   Max_rate = NULL,
-                                  Min_rent = NULL) {
+                                  Min_rent = NULL,
+                                  return. = c("new_ra", "sample_file", "sample_file.int")) {
+  
   #check sample file has correct format
   stopifnot(is.data.table(sample_file))
   cols_required <- c("rent",  "n_dependants", "has_partner", "is_homeowner", "lives_in_sharehouse")
@@ -26,12 +38,26 @@ model_rent_assistance <- function(sample_file,
   Is_homeowner <- sample_file[['is_homeowner']]
   Lives_in_sharehouse <- sample_file[['lives_in_sharehouse']]
   
-  rent_assistance(fortnightly_rent = Rent, 
-                  n_dependants = N_dependants,
-                  has_partner = Has_partner,
-                  is_homeowner = Is_homeowner,
-                  lives_in_sharehouse = Lives_in_sharehouse,
-                  .prop_rent_paid_by_RA = .Prop_rent_paid_by_RA,
-                  max_rate = Max_rate,
-                  min_rent = Min_rent)
+  baseline_ra <- rent_assistance(fortnightly_rent = Rent, 
+                            fy.year = baseline_fy,
+                            Date = baseline_Date,
+                            n_dependants = N_dependants,
+                            has_partner = Has_partner,
+                            is_homeowner = Is_homeowner,
+                            lives_in_sharehouse = Lives_in_sharehouse)
+  
+  new_ra <- rent_assistance(fortnightly_rent = Rent, 
+                            n_dependants = N_dependants,
+                            has_partner = Has_partner,
+                            is_homeowner = Is_homeowner,
+                            lives_in_sharehouse = Lives_in_sharehouse,
+                            .prop_rent_paid_by_RA = .Prop_rent_paid_by_RA,
+                            max_rate = Max_rate,
+                            min_rent = Min_rent)
+  
+  switch(return.,
+         "ra" = new_ra,
+         "sample_file" = set(sample_file, j = "new_ra", value = new_ra),
+         "sample_file.int" = set(sample_file, j = "new_ra", value = as.integer(new_ra)))
+  
 }
--- tests/testthat/test_model_rent_assistance.R
+++ tests/testthat/test_model_rent_assistance.R
@@ -2,18 +2,16 @@ context("Model rent assistance")
 
 test_that("Values", {
   sample<- CJ(rent = 1:500, n_dependants = 0, has_partner = FALSE, is_homeowner = FALSE, lives_in_sharehouse = FALSE)
-  expect_equal(model_rent_assistance(sample),
-               rent_assistance(sample))
-  expect_gt(sum(model_rent_assistance(sample, .Prop_rent_paid_by_RA = 0.8)),
-            sum(model_rent_assistance(sample, .Prop_rent_paid_by_RA = 0.7)))
-  expect_gt(sum(model_rent_assistance(sample, .Prop_rent_paid_by_RA = 0.75, Max_rate = 400, Min_rent = 0)),
-            sum(model_rent_assistance(sample, .Prop_rent_paid_by_RA = 0.75, Max_rate = 400, Min_rent = 100)))
-  expect_gt(sum(model_rent_assistance(sample, .Prop_rent_paid_by_RA = 0.75, Max_rate = 400, Min_rent = 100)),
-            sum(model_rent_assistance(sample, .Prop_rent_paid_by_RA = 0.75, Max_rate = 300, Min_rent = 100)))
+  expect_gt(sum(model_rent_assistance(sample, baseline_fy = "2018-19", .Prop_rent_paid_by_RA = 0.8, return. = "new_ra")),
+            sum(model_rent_assistance(sample, baseline_fy = "2018-19", .Prop_rent_paid_by_RA = 0.7, return. = "new_ra")))
+  expect_gt(sum(model_rent_assistance(sample, baseline_fy = "2018-19", .Prop_rent_paid_by_RA = 0.75, Max_rate = 400, Min_rent = 0, return. = "new_ra")),
+            sum(model_rent_assistance(sample, baseline_fy = "2018-19", .Prop_rent_paid_by_RA = 0.75, Max_rate = 400, Min_rent = 100, return. = "new_ra")))
+  expect_gt(sum(model_rent_assistance(sample, baseline_fy = "2018-19", .Prop_rent_paid_by_RA = 0.75, Max_rate = 400, Min_rent = 100, return. = "new_ra")),
+            sum(model_rent_assistance(sample, baseline_fy = "2018-19", .Prop_rent_paid_by_RA = 0.75, Max_rate = 300, Min_rent = 100, return. = "new_ra")))
 })
 
 test_that("Errors", {
   sample<- CJ(gsdfgfdg = 1:500, n_dependants = 0, has_partner = FALSE, is_homeowner = FALSE, lives_in_sharehouse = FALSE)
-  expect_error(model_rent_assistance(sample),
+  expect_error(model_rent_assistance(sample, baseline_fy = "2018-19"),
                regexp = "`sample_file` lacked the following required columns:\n\trent.\n")
 })
