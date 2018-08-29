86195b0c37439a34331ad62969c157072f66b35c
Matthew Katzen <matthewkatzen@gmail.com>
2018-08-29 10:58:14 +1000

    edit tests

--- tests/testthat/test_model_rent_assistance.R
+++ tests/testthat/test_model_rent_assistance.R
@@ -1,6 +1,7 @@
 context("Model rent assistance")
 
 test_that("Values", {
+  library(data.table)
   sample <- CJ(rent = 1:500, n_dependants = 0, has_partner = FALSE, is_homeowner = FALSE, lives_in_sharehouse = FALSE)
   expect_gt(sum(model_rent_assistance(sample, baseline_fy = "2018-19", .Prop_rent_paid_by_RA = 0.8, Max_rate = 300, Min_rent = 0)),
             sum(model_rent_assistance(sample, baseline_fy = "2018-19", .Prop_rent_paid_by_RA = 0.7, Max_rate = 300, Min_rent = 0)))
@@ -11,6 +12,7 @@ test_that("Values", {
 })
 
 test_that("Errors", {
+  library(data.table)
   sample <- CJ(gsdfgfdg = 1:500, n_dependants = 0, has_partner = FALSE, is_homeowner = FALSE, lives_in_sharehouse = FALSE)
   expect_error(model_rent_assistance(sample, baseline_fy = "2018-19", baseline_Date = "2018-01-01", .Prop_rent_paid_by_RA = 0.75, Max_rate = 300, Min_rent = 100),
                reg_exp = "only one of either `baseline_fy` or `baseline_Date` can be provided.")
