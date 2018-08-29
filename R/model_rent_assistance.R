f31dc6f9e901730bc6779ac71635692b538c69c7
Matthew Katzen <matthewkatzen@gmail.com>
2018-08-29 11:39:41 +1000

    Edit example

--- R/model_rent_assistance.R
+++ R/model_rent_assistance.R
@@ -16,16 +16,17 @@
 #' @examples
 #' 
 #' library(data.table)
-#' sample <-
-#'   CJ(rent = 1:500,
-#'      n_dependants = 0:3,
-#'      has_partner = 0:1 > 0,
-#'      is_homeowner = 0:1 > 0,
-#'      lives_in_sharehouse = 0:1 > 0) 
-#' model_rent_assistance(sample,
-#'                       .Prop_rent_paid_by_RA = 0.75,
-#'                       Max_rate = 500,
-#'                       Min_rent = 100)
+# sample <-
+#   CJ(rent = 1:500,
+#      n_dependants = 0:3,
+#      has_partner = 0:1 > 0,
+#      is_homeowner = 0:1 > 0,
+#      lives_in_sharehouse = 0:1 > 0)
+# model_rent_assistance(sample,
+#                       baseline_fy = "2018-19",
+#                       .Prop_rent_paid_by_RA = 0.75,
+#                       Max_rate = 500,
+#                       Min_rent = 100)
 
 model_rent_assistance <- function(sample_file,
                                   baseline_fy = NULL,
