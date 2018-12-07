2e3ef17ce6fe325c053b5c9a7bf40b4f0a5be79c
HughParsonage <hugh.parsonage@gmail.com>
2018-12-08 04:02:34 +1100

    Add coverage

--- tests/testthat/test_awote.R
+++ tests/testthat/test_awote.R
@@ -7,3 +7,8 @@ test_that("Error handling", {
   expect_error(awote(isAdult = NA))
   expect_error(awote(isOrdinary = NA))
 })
+
+test_that("Message", {
+  expect_message(awote(), 
+                 regexp = "both NULL so using")
+})
