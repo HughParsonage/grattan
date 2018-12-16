055ab51e0cd85899dc433a3b9abfb99b80f3584a
HughParsonage <hugh.parsonage@gmail.com>
2018-12-16 23:13:05 +1100

    Test #158

---
+++ tests/testthat/test_incometax_dotsATO_sfa.R
@@ -0,0 +1,22 @@
+context("sample files all")
+
+test_that("sfa", {
+  skip_if_not_installed("taxstats", minimum_version = "0.1.0.1415")
+  library(data.table)
+  library(taxstats)
+  library(magrittr)
+  sfa <- get_sample_files_all2()
+  
+  sfa200304 <- 
+    sfa[fy_year == "2003-04"] %>%
+    .[, tax := income_tax(Taxable_Income, "2003-04", .dots.ATO = .SD)]
+
+  
+  s0304 <- copy(sample_file_0304)
+  s0304[, tax := income_tax(Taxable_Income, "2003-04", .dots.ATO = .SD)]  
+  
+  expect_equal(s0304[["Ind"]], s0304[["Ind"]]) # Just to check order-preservation
+  expect_equal(s0304[["tax"]], sfa200304[["tax"]])
+  
+})
+
