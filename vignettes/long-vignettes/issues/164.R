0fc7e7144c36b7247601bd3871551e4762b34d3d
HughParsonage <hugh.parsonage@gmail.com>
2019-01-25 14:27:55 +1100

    Different colours as requested by @DaniWood

--- vignettes/long-vignettes/issues/164.R
+++ vignettes/long-vignettes/issues/164.R
@@ -3,16 +3,18 @@ library(SampleFile1516)
 library(data.table)
 library(magrittr)
 library(hutils)
+library(taxstats)
+library(ggplot2)
 
 p <- 
-  s1516[, .(avgTaxableIncome = mean(Taxable_Income)), keyby = .(age_range, Gender)] %>%
-  .[age_range_decoder, on = "age_range"] %>%
+  sample_file_1516[, .(avgTaxableIncome = mean(Taxable_Income)), keyby = .(age_range, Gender)] %>%
   .[, Sex := if_else(Gender == 1, "Female", "Male")] %>%
   .[, Age := 70 - 5 * age_range] %>%
   grplot(aes(x = Age,
              y = avgTaxableIncome,
              group = Sex, 
-             color = Sex)) + 
+             color = Sex),
+         reverse = FALSE) + 
   geom_line() + 
   theme(legend.position = "bottom",
         legend.title = element_blank()) +
