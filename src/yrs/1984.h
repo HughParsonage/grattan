fbd97bbe6369bb14a1d971c5883aae9c25a89bcf
HughParsonage <hugh.parsonage@gmail.com>
2020-09-29 03:58:52 +1000

    Add 1984-1989

--- src/yrs/1984.h
+++ src/yrs/1984.h
@@ -1,6 +1,11 @@
 #ifndef grattan_1984_H
 #define grattan_1984_H
 
+constexpr double ORD_TAX_BRACK_1984[4] = {0, 4595, 19500, 35788};
+constexpr double ORD_TAX_RATES_1984[4] = {0, 0.30, 0.46, 0.60};
+constexpr double ML_LWR_THR_UP_PER_CHILD_1984 =  1100;
 constexpr double ML_LWR_THRESHOLD_SINGLE_1984 =  6699;
 constexpr double ML_UPR_THRESHOLD_SINGLE_1984 =  7051;
+constexpr double ML_LWR_THRESHOLD_FAMILY_1984 = 11141;
+constexpr double ML_UPR_THRESHOLD_FAMILY_1984 = 11728;
 #endif
