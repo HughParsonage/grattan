af5b5487451de032d035a043b3bedf0dfeaea188
HughParsonage <hugh.parsonage@gmail.com>
2021-07-10 15:52:40 +1000

    Update guides

--- data-raw/put-guides-dss-gov-au.R
+++ data-raw/put-guides-dss-gov-au.R
@@ -1,1 +1,6 @@
-shell("wget64 -r --no-parent --page-requisites --convert-links http://guides.dss.gov.au")
+wwget64 <- Sys.which("wget64")
+if (!nzchar(wwget64)) {
+  wwget64 <- r'{C:\Program Files\Git\mingw64\bin\wget64.exe}'
+}
+
+shell(paste(shQuote(wwget64), "-r --no-parent --page-requisites --convert-links http://guides.dss.gov.au"))
