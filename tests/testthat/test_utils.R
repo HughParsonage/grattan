d4ea5c179bd8a716f55cb61949bfacd60d546bae
HughParsonage <hugh.parsonage@gmail.com>
2022-06-27 03:42:33 +1000

    Correct use of lito API

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -62,7 +62,7 @@ test_that("fast selector", {
 
 test_that("koffset", {
   expect_equal(koffset(37000 + 1:10, c(0, 37e3, 66667, Inf), c(445, 445, 0, 0)), 
-               lito(37000 + 1:10))
+               lito(37000 + 1:10, "2015-16"))
 })
 
 test_that("Switch", {
