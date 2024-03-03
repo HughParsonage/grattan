45170c9271ec797420be9e49aefd5806e058725d
HughParsonage <hugh.parsonage@gmail.com>
2024-03-03 12:28:20 +1100

    Try insisting on not memoizing

--- src/Offset.c
+++ src/Offset.c
@@ -271,6 +271,7 @@ void do_multiOffsets(double * restrict ansp,
   
   bool dont_memoize = 
     N < 1000 || n_fmem > 1048576 || (apply && any_refundable && !all_refundable);
+  dont_memoize = true;
   
   // fmem[i] is the (total) offsets for [income] i
   double * fmem = dont_memoize ? NULL : malloc(sizeof(double) * (n_fmem + 1u));
